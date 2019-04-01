0ae72317004268d521eb516bbba49e81efc375a9
Zhivaev Artem <zhivaev993@gmail.com>
2019-04-01 19:37:41 +0300

    new tests for lab1

--- modules/complex-number/test/test_zhivaev_artem_complex_number.cpp
+++ modules/complex-number/test/test_zhivaev_artem_complex_number.cpp
@@ -6,7 +6,7 @@
 
 TEST(Zhivaev_Artem_ComplexNumberTest, Can_Create_Negative_Real) {
     // Arrange
-    double re = -4719719.7568345;
+    double re = -9.45;
     double im = 0.0;
 
     // Act
@@ -14,6 +14,57 @@ TEST(Zhivaev_Artem_ComplexNumberTest, Can_Create_Negative_Real) {
 
     // Assert
     EXPECT_EQ(re, z.getRe());
+    EXPECT_EQ(im, z.getIm());
+}
+
+TEST(Zhivaev_Artem_ComplexNumberTest, Can_Create_Negative_Imagine) {
+    // Arrange
+    double re = 0.0;
+    double im = -9.45;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_EQ(re, z.getRe());
+    EXPECT_EQ(im, z.getIm());
+}
+
+TEST(Zhivaev_Artem_ComplexNumberTest, Can_Copy) {
+    // Arrange
+    double re = 123.123;
+    double im = 321.321;
+
+    // Act
+    ComplexNumber z1(re, im);
+    ComplexNumber z2(z1);
+
+    // Assert
+    EXPECT_EQ(re, z2.getRe());
+    EXPECT_EQ(im, z2.getIm());
+}
+
+TEST(Zhivaev_Artem_ComplexNumberTest, Can_SetRe) {
+    // Arrange
+    double re = -123.123;
+
+    // Act
+    ComplexNumber z;
+    z.setRe(re);
+
+    // Assert
+    EXPECT_EQ(re, z.getRe());
+}
+
+TEST(Zhivaev_Artem_ComplexNumberTest, Can_SetIm) {
+    // Arrange
+    double im = -123.123;
+
+    // Act
+    ComplexNumber z;
+    z.setIm(im);
+
+    // Assert
     EXPECT_EQ(im, z.getIm());
 }
 
