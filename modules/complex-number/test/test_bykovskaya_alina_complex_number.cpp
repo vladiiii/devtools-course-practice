521c2eb3ad9a540f51b207c13791cf2edc37faf2
Alina Bykovskaya <43119853+alinaut@users.noreply.github.com>
2019-04-06 16:46:16 +0300

    more tests for add, diff & division operators

--- modules/complex-number/test/test_bykovskaya_alina_complex_number.cpp
+++ modules/complex-number/test/test_bykovskaya_alina_complex_number.cpp
@@ -34,3 +34,41 @@ TEST(Bykovskaya_Alina_ComplexNumberTest, Can_Create_By_Copy_Constructor) {
     EXPECT_EQ(a.getRe(), b.getRe());
     EXPECT_EQ(a.getIm(), b.getIm());
 }
+
+TEST(Bykovskaya_Alina_ComplexNumberTest,
+                            Can_Add_More_Than_Two_Complex) {
+    // Arrange
+    ComplexNumber a(1., 1.);
+    ComplexNumber b(2., 2.);
+    ComplexNumber expect(6., 6.);
+
+    // Act
+    ComplexNumber c = a + a + b + b;
+
+    // Assert
+    EXPECT_EQ(expect.getRe(), c.getRe());
+    EXPECT_EQ(expect.getIm(), c.getIm());
+}
+
+TEST(Bykovskaya_Alina_ComplexNumberTest,
+                        Can_Difference_More_Than_Two_Complex) {
+    // Arrange
+    ComplexNumber a(20., 20.);
+    ComplexNumber b(2., 2.);
+    ComplexNumber expect(14., 14.);
+
+    // Act
+    ComplexNumber c = a - b - b - b;
+
+    // Assert
+    EXPECT_EQ(expect.getRe(), c.getRe());
+    EXPECT_EQ(expect.getIm(), c.getIm());
+}
+
+TEST(Bykovskaya_Alina_ComplexNumberTest, cant_division_by_zero) {
+    // Arrange
+    ComplexNumber a(5.0, 5.0), b(-5.0, -5.0), c(5.6, 9.8);
+
+    // Act & Assert
+    ASSERT_ANY_THROW((c + c) / (a + b));
+}
