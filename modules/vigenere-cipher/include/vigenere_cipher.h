d7b77023206a6ed07df0c2dd08ea1c391dcdc459
Arseniy Obolenskiy <gooddoog@student.su>
2019-04-06 23:52:35 +0300

    Code style fix

--- modules/vigenere-cipher/include/vigenere_cipher.h
+++ modules/vigenere-cipher/include/vigenere_cipher.h
@@ -5,8 +5,10 @@
 
 class VigenereCipher {
  public:
-    static std::string encrypt(const std::string &message, const std::string& key);
-    static std::string decrypt(const std::string &message, const std::string& key);
+    static std::string encrypt(const std::string &message,
+                               const std::string& key);
+    static std::string decrypt(const std::string &message,
+                               const std::string& key);
 };
 
 #endif  // MODULES_VIGENERE_CIPHER_INCLUDE_VIGENERE_CIPHER_H_
