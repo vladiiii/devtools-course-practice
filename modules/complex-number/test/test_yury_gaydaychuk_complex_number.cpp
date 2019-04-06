65d7fc55f35cd51d63210d1b92511b4f310a502c
Yury Gaydaychuk <agri1987cola@gmail.com>
2019-04-06 17:22:41 +0300

    2nd test added

--- modules/complex-number/test/test_yury_gaydaychuk_complex_number.cpp
+++ modules/complex-number/test/test_yury_gaydaychuk_complex_number.cpp
@@ -15,3 +15,15 @@ TEST(Gaydaychuk_Yury_ComplexNumberTest, Can_Multiple_One) {
     // Assert
     EXPECT_EQ(z2, z3);
 }
+
+TEST(Gaydaychuk_Yury_ComplexNumberTest, Solve_General_Equation) {
+    // Arrange
+    ComplexNumber im_one(0.0, 1.0);
+    ComplexNumber minus_one(-1.0, 0.0);
+
+    // Act
+    ComplexNumber z = im_one * im_one;
+
+    // Assert
+    EXPECT_EQ(minus_one, z);
+}
