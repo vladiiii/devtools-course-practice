7bf6f86c25fe1b88cc5e7ab539f313ed0cb4169d
DamirV <v.d.d.8@ya.ru>
2019-04-07 16:37:27 +0300

    Added new tests for lab1

--- modules/complex-number/test/test_valiullin_damir_complex_number.cpp
+++ modules/complex-number/test/test_valiullin_damir_complex_number.cpp
@@ -29,3 +29,44 @@ TEST(Valiullin_Damir_ComplexNumberTest, Complex_numbers_are_equal) {
 	// Assert
 	EXPECT_EQ(z,z1);
 }
+
+TEST(Valiullin_Damir_ComplexNumberTest, Complex_numbers_are_not_equal) {
+	// Arrange
+	double re = 10.0;
+	double im = 20.0;
+
+	// Act
+	ComplexNumber z(re, im);
+	ComplexNumber z1(z);
+	z1.setIm(1.0);
+
+	// Assert
+	EXPECT_NE(z,z1);
+}
+
+TEST(Valiullin_Damir_ComplexNumberTest, Can_not_divide_by_zero) {
+	// Arrange
+	double re = 0.0;
+	double im = 0.0;
+
+	// Act
+	ComplexNumber z(re, im);
+	ComplexNumber z1(10.0, 20.0);
+
+	// Assert
+	ASSERT_ANY_THROW(z1/z);
+}
+
+TEST(Valiullin_Damir_ComplexNumberTest, Multiplying_by_one_gives_the_same_number) {
+	// Arrange
+	double re = 10.0;
+	double im = 20.0;
+
+	// Act
+	ComplexNumber z(re, im);
+	ComplexNumber copyz(z);
+	ComplexNumber z1(1.0, 0.0);
+
+	// Assert
+	EXPECT_EQ(copyz, z*z1);
+}
