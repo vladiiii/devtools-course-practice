80d8e972f70bfcf15c3a719578e0bc478133c736
DamirV <v.d.d.8@ya.ru>
2019-04-07 17:03:37 +0300

    Added new tests for lab1

--- modules/complex-number/test/test_valiullin_damir_complex_number.cpp
+++ modules/complex-number/test/test_valiullin_damir_complex_number.cpp
@@ -5,68 +5,66 @@
 #include "include/complex_number.h"
 
 TEST(Valiullin_Damir_ComplexNumberTest, Can_Create_Zero) {
-	// Arrange
-	double re = 0.0;
-	double im = 0.0;
+    // Arrange
+    double re = 0.0;
+    double im = 0.0;
 
-	// Act
-	ComplexNumber z(re, im);
+    // Act
+    ComplexNumber z(re, im);
 
-	// Assert
-	EXPECT_EQ(re, z.getRe());
-	EXPECT_EQ(im, z.getIm());
+    // Assert
+    EXPECT_EQ(re, z.getRe());
+    EXPECT_EQ(im, z.getIm());
 }
 
 TEST(Valiullin_Damir_ComplexNumberTest, Complex_numbers_are_equal) {
-	// Arrange
-	double re = 0.0;
-	double im = 0.0;
+    // Arrange
+    double re = 0.0;
+    double im = 0.0;
 
-	// Act
-	ComplexNumber z(re, im);
-	ComplexNumber z1(z);
+    // Act
+    ComplexNumber z(re, im);
+    ComplexNumber z1(z);
 
-	// Assert
-	EXPECT_EQ(z,z1);
+    // Assert
+    EXPECT_EQ(z,z1);
 }
 
-TEST(Valiullin_Damir_ComplexNumberTest, Complex_numbers_are_not_equal) {
-	// Arrange
-	double re = 10.0;
-	double im = 20.0;
+TEST(Valiullin_Damir_ComplexNumberTest, Can_multiply_by_zero) {
+    // Arrange
+    double re = 10.0;
+    double im = 20.0;
 
-	// Act
-	ComplexNumber z(re, im);
-	ComplexNumber z1(z);
-	z1.setIm(1.0);
+    // Act
+    ComplexNumber z(re, im);
+    ComplexNumber z1(0.0, 0.0);
 
-	// Assert
-	EXPECT_NE(z,z1);
+    // Assert
+    EXPECT_EQ(z1,z*z1);
 }
 
 TEST(Valiullin_Damir_ComplexNumberTest, Can_not_divide_by_zero) {
-	// Arrange
-	double re = 0.0;
-	double im = 0.0;
+    // Arrange
+    double re = 0.0;
+    double im = 0.0;
 
-	// Act
-	ComplexNumber z(re, im);
-	ComplexNumber z1(10.0, 20.0);
+    // Act
+    ComplexNumber z(re, im);
+    ComplexNumber z1(10.0, 20.0);
 
-	// Assert
-	ASSERT_ANY_THROW(z1/z);
+    // Assert
+    ASSERT_ANY_THROW(z1/z);
 }
 
 TEST(Valiullin_Damir_ComplexNumberTest, Multiplying_by_one_gives_the_same_number) {
-	// Arrange
-	double re = 10.0;
-	double im = 20.0;
+    // Arrange
+    double re = 10.0;
+    double im = 20.0;
 
-	// Act
-	ComplexNumber z(re, im);
-	ComplexNumber copyz(z);
-	ComplexNumber z1(1.0, 0.0);
+    // Act
+    ComplexNumber z(re, im);
+    ComplexNumber z1(1.0, 0.0);
 
-	// Assert
-	EXPECT_EQ(copyz, z*z1);
+    // Assert
+    EXPECT_EQ(z, z*z1);
 }
