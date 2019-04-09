25c1cfb5504a6ee71b3a672c352086465d9c988d
PinDanil <drobrorobro@gmail.com>
2019-04-09 19:10:10 +0300

    F*cking tabs

--- modules/complex-number/test/test_pinaev_danil_complex_number.cpp
+++ modules/complex-number/test/test_pinaev_danil_complex_number.cpp
@@ -51,53 +51,53 @@ TEST(Pinaev_Danil_ComplexNumberTest, Can_Not_Division_On_Zero_Complex_Number) {
 
 
 TEST(Pinaev_Danil_ComplexNumberTest, Have_A_Defauld_Value) {
-	// Arrange
-	ComplexNumber a;
-	ComplexNumber b;
+    // Arrange
+    ComplexNumber a;
+    ComplexNumber b;
 
-	// Assert
-	ASSERT_EQ(a, b);
+    // Assert
+    ASSERT_EQ(a, b);
 }
 
 
 TEST(Pinaev_Danil_ComplexNumberTest, Defauld_Value_Is_Zero_Value) {
-	// Arrange
-	ComplexNumber a(0, 0);
-	ComplexNumber b;
+    // Arrange
+    ComplexNumber a(0, 0);
+    ComplexNumber b;
 
-	// Assert
-	ASSERT_EQ(a, b);
+    // Assert
+    ASSERT_EQ(a, b);
 }
 
 
 TEST(Pinaev_Danil_ComplexNumberTest, Can_Create_Arr_Complex_Numbers) {
-	// Assert
-	ASSERT_NO_THROW(ComplexNumber A[5]);
+    // Assert
+    ASSERT_NO_THROW(ComplexNumber A[5]);
 }
 
 
 TEST(Pinaev_Danil_ComplexNumberTest, Can_Add_To_Arr) {
-	// Arrange
-	ComplexNumber a(1, -2);
-	ComplexNumber A[5];
+    // Arrange
+    ComplexNumber a(1, -2);
+    ComplexNumber A[5];
 
-	// Assert
-	ASSERT_NO_THROW(A[0] = a);
+    // Assert
+    ASSERT_NO_THROW(A[0] = a);
 }
 
 
 TEST(Pinaev_Danil_ComplexNumberTest, Some_Weird_Acts_With_Arr) {
-	// Arrange
-	ComplexNumber a, b;
-	ComplexNumber A[4];
-	A[0] = ComplexNumber(3, 4);
-	A[1] = ComplexNumber(-3, -4);
-	A[2] = ComplexNumber(5, 6);
-	A[3] = ComplexNumber(7, 8);
-	
-	// Act
-	b = ((A[0] + A[1]) / (A[2] * A[3]));
+    // Arrange
+    ComplexNumber a, b;
+    ComplexNumber A[4];
+    A[0] = ComplexNumber(3, 4);
+    A[1] = ComplexNumber(-3, -4);
+    A[2] = ComplexNumber(5, 6);
+    A[3] = ComplexNumber(7, 8);
 
-	// Assert
-	ASSERT_EQ(a, b);
+    // Act
+    b = ((A[0] + A[1]) / (A[2] * A[3]));
+
+    // Assert
+    ASSERT_EQ(a, b);
 }
