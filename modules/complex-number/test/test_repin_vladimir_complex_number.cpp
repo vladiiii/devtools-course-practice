14fc43520f18142b0883043c07ea69a073d83a17
Repin Vladimir <mezotaken@yandex.ru>
2019-04-09 06:26:23 +0300

    lab1 tests update
    Redundant test removed, added unique

--- modules/complex-number/test/test_repin_vladimir_complex_number.cpp
+++ modules/complex-number/test/test_repin_vladimir_complex_number.cpp
@@ -4,69 +4,90 @@
 
 #include "include/complex_number.h"
 
-TEST(Repin_Vladimir_ComplexNumberTest, throws_on_division_by_zero) {
+TEST(Repin_Vladimir_ComplexNumberTest, can_get_zero_by_sum) {
     // Arrange
-    double re1 = 0.0;
-    double im1 = 0.0;
-    double re2 = 1.0;
-    double im2 = 1.0;
-
-    // Act
+    double re1 = 3.0;
+    double im1 = 2.0;
+    double re2 = -3.0;
+    double im2 = -2.0;
     ComplexNumber z1(re1, im1);
     ComplexNumber z2(re2, im2);
 
-
-    // Assert
-
-    ASSERT_ANY_THROW(z2 / z1);
-}
-
-TEST(Repin_Vladimir_ComplexNumberTest, comparing_with_itself_is_correct) {
-    // Arrange
-    double re = 3.0;
-    double im = 7.0;
-
-
     // Act
-    ComplexNumber z(re, im);
-
+    ComplexNumber z = z1 + z2;
 
     // Assert
-
-    EXPECT_EQ(true, z == z);
+    ComplexNumber expected_z(0.0, 0.0);
+    EXPECT_EQ(expected_z, z);
 }
 
-TEST(Repin_Vladimir_ComplexNumberTest, multiplying_is_correct) {
+TEST(Repin_Vladimir_ComplexNumberTest, can_get_one_by_division) {
     // Arrange
     double re1 = 3.0;
     double im1 = 7.0;
-
-    double re2 = 7.0;
-    double im2 = 3.0;
-
-    // Act
+    double re2 = 3.0;
+    double im2 = 7.0;
     ComplexNumber z1(re1, im1);
     ComplexNumber z2(re2, im2);
-    ComplexNumber res = z1*z2;
-
-    // Assert
-
-    EXPECT_EQ(0.0, res.getRe());
-    EXPECT_EQ(58.0, res.getIm());
-}
-
-TEST(Repin_Vladimir_ComplexNumberTest, assign_is_correct) {
-    // Arrange
-    double re = 11.0;
-    double im = 2.0;
 
     // Act
-    ComplexNumber z1(re, im);
-    ComplexNumber z2;
-    z2 = z1;
+    ComplexNumber z = z1 / z2;
 
     // Assert
+    ComplexNumber expected_z(1.0, 0.0);
+    EXPECT_EQ(expected_z, z);
+}
 
-    EXPECT_EQ(z1.getRe(), z2.getRe());
-    EXPECT_EQ(z1.getIm(), z2.getIm());
+TEST(Repin_Vladimir_ComplexNumberTest, multiple_assign_is_correct) {
+    // Arrange
+    double re = 3.0;
+    double im = 7.0;
+    ComplexNumber z(re, im);
+    ComplexNumber z1, z2;
+
+    // Act 
+    z2 = z1 = z;
+
+    // Assert
+    ComplexNumber expected_z(3.0, 7.0);
+    EXPECT_EQ(expected_z, z1);
+    EXPECT_EQ(expected_z, z2);
+}
+
+TEST(Repin_Vladimir_ComplexNumberTest, sum_operands_stay_unchanged) {
+    // Arrange
+    double re1 = 3.0;
+    double im1 = 7.0;
+    double re2 = 3.0;
+    double im2 = 7.0;
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+
+    // Act 
+    ComplexNumber z = z1 + z2;
+
+    // Assert
+    ComplexNumber expected_z1(3.0, 7.0);
+    ComplexNumber expected_z2(3.0, 7.0);
+    EXPECT_EQ(expected_z1, z1);
+    EXPECT_EQ(expected_z2, z2);
+}
+
+TEST(Repin_Vladimir_ComplexNumberTest, division_operands_stay_unchanged) {
+    // Arrange
+    double re1 = 3.0;
+    double im1 = 7.0;
+    double re2 = 3.0;
+    double im2 = 7.0;
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+
+    // Act 
+    ComplexNumber z = z1 / z2;
+
+    // Assert
+    ComplexNumber expected_z1(3.0, 7.0);
+    ComplexNumber expected_z2(3.0, 7.0);
+    EXPECT_EQ(expected_z1, z1);
+    EXPECT_EQ(expected_z2, z2);
 }
