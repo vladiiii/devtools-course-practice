51a54e1659d2e477e3876723d83d51eab020973c
TheSanchouz <thesanchouz@gmail.com>
2019-04-09 17:53:35 +0300

    Add new test for lab1 - Multiply By Conjugate Gives Radius

--- modules/complex-number/test/test_trykin_alexander_complex_number.cpp
+++ modules/complex-number/test/test_trykin_alexander_complex_number.cpp
@@ -10,11 +10,11 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Re_One) {
     ComplexNumber re_one(1, 0);
 
     // Act
-    z = z / re_one;
+    ComplexNumber res = z / re_one;
 
     // Assert
     ComplexNumber expected_z(12.4, 51);
-    EXPECT_EQ(expected_z, z);
+    EXPECT_EQ(expected_z, res);
 }
 
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Im_One) {
@@ -23,11 +23,11 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Im_One) {
     ComplexNumber im_one(0, 1);
 
     // Act
-    z = z / im_one;
+    ComplexNumber res = z / im_one;
 
     // Assert
     ComplexNumber expected_z(5.2, -5.4);
-    EXPECT_EQ(expected_z, z);
+    EXPECT_EQ(expected_z, res);
 }
 
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Re_One) {
@@ -36,11 +36,11 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Re_One) {
     ComplexNumber re_one(1, 0);
 
     // Act
-    z = z * re_one;
+    ComplexNumber res = z * re_one;
 
     // Assert
     ComplexNumber expected_z(-2.9, -4.8);
-    EXPECT_EQ(expected_z, z);
+    EXPECT_EQ(expected_z, res);
 }
 
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Im_One) {
@@ -49,9 +49,22 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Im_One) {
     ComplexNumber im_one(0, 1);
 
     // Act
-    z = z * im_one;
+    ComplexNumber res = z * im_one;
 
     // Assert
     ComplexNumber expected_z(-10.1, -12.3);
-    EXPECT_EQ(expected_z, z);
+    EXPECT_EQ(expected_z, res);
+}
+
+TEST(Trykin_Alexander_ComplexNumberTest, Multiply_By_Conjugate_Gives_Radius) {
+    // Arrange
+    ComplexNumber z1(5, 2);
+    ComplexNumber z2(5, -2);
+
+    // Act
+    ComplexNumber res = z1 * z2;
+
+    // Assert
+    float radius = z1.getRe() * z1.getRe() + z2.getIm() * z2.getIm();
+    EXPECT_FLOAT_EQ(radius, res.getRe());
 }
