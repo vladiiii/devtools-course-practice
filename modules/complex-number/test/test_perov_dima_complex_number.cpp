99a1ab2a46e203428a47113bef7bce50ddb53621
Dima Perov <diper1998@users.noreply.github.com>
2019-04-09 09:12:10 +0300

    Create test_perov_dima_complex_number.cpp

---
+++ modules/complex-number/test/test_perov_dima_complex_number.cpp
@@ -0,0 +1,72 @@
+// Copyright 2019 Perov Dima
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Perov_Dima_ComplexNumberTest, Can_Combination_Operations) {
+    // Arrange
+    double re1 = 1.0;
+    double im1 = 1.0;
+    double re2 = 1.0;
+    double im2 = -1.0;
+    double re3 = -1.0;
+    double im3 = 1.0;
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3(re3, im3);
+    ComplexNumber res(0, 0);
+
+    // Act
+    res = z3+z1*z2;
+
+
+    // Assert
+    double expectedRe = 1;
+    double expectedIm = 1;
+    EXPECT_EQ(expectedRe, res.getRe());
+    EXPECT_EQ(expectedIm, res.getIm());
+
+}
+
+TEST(Perov_Dima_ComplexNumberTest, Can_Unary_Minus) {
+    // Arrange
+    double re1 = 1.0;
+    double im1 = 1.0;
+    ComplexNumber z1(re1, im1);
+    
+    // Act
+    z1 = -z1;
+
+
+    // Assert
+    double expectedRe = -1;
+    double expectedIm = -1;
+    ComplexNumber expectedZ(expectedRe, expectedIm);
+    EXPECT_EQ(expectedZ, z1);
+    
+}
+
+TEST(Perov_Dima_ComplexNumberTest, Check_Comparison_Priority) {
+    // Arrange
+    double re1 = 1.0;
+    double im1 = 1.0;
+    double re2 = 1.0;
+    double im2 = -1.0;
+    double re3 = 0.0;
+    double im3 = 2.0;
+    bool res = true;
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber z3(re3, im3);
+    
+    // Act
+    res = z1 == z2+z3;
+
+
+    // Assert
+    bool expectedRes = true;
+    EXPECT_EQ(expectedRes, res);
+    
+}
+
