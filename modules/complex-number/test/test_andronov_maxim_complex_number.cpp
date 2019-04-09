9a0905a4a64f096fb9dcf7803b319214d4053a4c
and7mw <and7mw@gmail.com>
2019-04-09 21:33:41 +0300

    added tests for const by default and div several

--- modules/complex-number/test/test_andronov_maxim_complex_number.cpp
+++ modules/complex-number/test/test_andronov_maxim_complex_number.cpp
@@ -4,6 +4,17 @@
 
 #include "include/complex_number.h"
 
+TEST(Andronov_Maxim_ComplexNumberTest, Can_Create_By_Default) {
+    // Arrange
+    ComplexNumber expected_z(0.0, 0.0);
+
+    // Act
+    ComplexNumber z;
+
+    // Assert
+    EXPECT_EQ(expected_z, z);
+}
+
 TEST(Andronov_Maxim_ComplexNumberTest, Can_Create_Copy) {
     // Arrange
     ComplexNumber expected_z(10.0, 20.0);
@@ -67,6 +78,21 @@ TEST(Andronov_Maxim_ComplexNumberTest, Can_Multiplication_Several) {
 
     // Act
     z4 = z3 * z2 * z1;
+
+    // Assert
+    EXPECT_EQ(expected_z, z4);
+}
+
+TEST(Andronov_Maxim_ComplexNumberTest, Can_Division_Several) {
+    // Arrange
+    ComplexNumber z1(2.0, 3.0);
+    ComplexNumber z2(-50.0, 100.0);
+    ComplexNumber z3(-400.0, 50.0);
+    ComplexNumber z4;
+    ComplexNumber expected_z(1.0, 0.0);
+
+    // Act
+    z4 = z3 / z2 / z1;
 
     // Assert
     EXPECT_EQ(expected_z, z4);
