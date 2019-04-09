91d1c60ea1edc4f72c1618e0fc9c09d7650bcd5a
TheSanchouz <thesanchouz@gmail.com>
2019-04-09 18:08:53 +0300

    Updated tests

--- modules/complex-number/test/test_trykin_alexander_complex_number.cpp
+++ modules/complex-number/test/test_trykin_alexander_complex_number.cpp
@@ -13,8 +13,8 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Re_One) {
     ComplexNumber res = z / re_one;
 
     // Assert
-    ComplexNumber expected_z(12.4, 51);
-    EXPECT_EQ(expected_z, res);
+    ComplexNumber expected_res(12.4, 51);
+    EXPECT_EQ(expected_res, res);
 }
 
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Im_One) {
@@ -26,8 +26,8 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Im_One) {
     ComplexNumber res = z / im_one;
 
     // Assert
-    ComplexNumber expected_z(5.2, -5.4);
-    EXPECT_EQ(expected_z, res);
+    ComplexNumber expected_res(5.2, -5.4);
+    EXPECT_EQ(expected_res, res);
 }
 
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Re_One) {
@@ -39,8 +39,8 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Re_One) {
     ComplexNumber res = z * re_one;
 
     // Assert
-    ComplexNumber expected_z(-2.9, -4.8);
-    EXPECT_EQ(expected_z, res);
+    ComplexNumber expected_res(-2.9, -4.8);
+    EXPECT_EQ(expected_res, res);
 }
 
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Im_One) {
@@ -52,8 +52,8 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Im_One) {
     ComplexNumber res = z * im_one;
 
     // Assert
-    ComplexNumber expected_z(-10.1, -12.3);
-    EXPECT_EQ(expected_z, res);
+    ComplexNumber expected_res(-10.1, -12.3);
+    EXPECT_EQ(expected_res, res);
 }
 
 TEST(Trykin_Alexander_ComplexNumberTest, Multiply_By_Conjugate_Gives_Radius) {
