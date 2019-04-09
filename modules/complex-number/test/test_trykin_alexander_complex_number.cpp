da7dde9d25acccbcb097d2c1f33c08703f7615f9
TheSanchouz <thesanchouz@gmail.com>
2019-04-09 17:36:44 +0300

    Add new test for lab1 - Can Multiply By Im One

--- modules/complex-number/test/test_trykin_alexander_complex_number.cpp
+++ modules/complex-number/test/test_trykin_alexander_complex_number.cpp
@@ -7,10 +7,10 @@
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Re_One) {
     // Arrange
     ComplexNumber z(12.4, 51);
-    ComplexNumber one(1, 0);
+    ComplexNumber re_one(1, 0);
 
     // Act
-    z = z / one;
+    z = z / re_one;
 
     // Assert
     ComplexNumber expected_z(12.4, 51);
@@ -20,10 +20,10 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Re_One) {
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Im_One) {
     // Arrange
     ComplexNumber z(5.4, 5.2);
-    ComplexNumber one(0, 1);
+    ComplexNumber im_one(0, 1);
 
     // Act
-    z = z / one;
+    z = z / im_one;
 
     // Assert
     ComplexNumber expected_z(5.2, -5.4);
@@ -33,12 +33,25 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Im_One) {
 TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Re_One) {
     // Arrange
     ComplexNumber z(-2.9, -4.8);
-    ComplexNumber one(1, 0);
+    ComplexNumber re_one(1, 0);
 
     // Act
-    z = z * one;
+    z = z * re_one;
 
     // Assert
     ComplexNumber expected_z(-2.9, -4.8);
+    EXPECT_EQ(expected_z, z);
+}
+
+TEST(Trykin_Alexander_ComplexNumberTest, Can_Multiply_By_Im_One) {
+    // Arrange
+    ComplexNumber z(-12.3, 10.1);
+    ComplexNumber im_one(0, 1);
+
+    // Act
+    z = z * im_one;
+
+    // Assert
+    ComplexNumber expected_z(-10.1, -12.3);
     EXPECT_EQ(expected_z, z);
 }
