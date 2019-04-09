bf3b296dd2289f9c9d96ce1984fffd62c90ab10c
TheSanchouz <thesanchouz@gmail.com>
2019-04-09 17:27:11 +0300

    Add new test for lab1 - Can Divide By Im One

--- modules/complex-number/test/test_trykin_alexander_complex_number.cpp
+++ modules/complex-number/test/test_trykin_alexander_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_One) {
+TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Re_One) {
     // Arrange
     ComplexNumber z(12.4, 51);
     ComplexNumber one(1, 0);
@@ -14,6 +14,19 @@ TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_One) {
 
     // Assert
     ComplexNumber expected_z(12.4, 51);
+    EXPECT_EQ(expected_z, z);
+}
+
+TEST(Trykin_Alexander_ComplexNumberTest, Can_Divide_By_Im_One) {
+    // Arrange
+    ComplexNumber z(5.4, 5.2);
+    ComplexNumber one(0, 1);
+
+    // Act
+    z = z / one;
+
+    // Assert
+    ComplexNumber expected_z(5.2, -5.4);
     EXPECT_EQ(expected_z, z);
 }
 
