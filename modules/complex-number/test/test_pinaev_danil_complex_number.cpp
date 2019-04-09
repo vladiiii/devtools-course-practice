ba482705f32457aa925cebc8b68c4e3c33dc8efb
PinDanil <drobrorobro@gmail.com>
2019-04-09 18:46:33 +0300

    Added some new tests and fixed style problem.

--- modules/complex-number/test/test_pinaev_danil_complex_number.cpp
+++ modules/complex-number/test/test_pinaev_danil_complex_number.cpp
@@ -4,13 +4,13 @@
 
 #include "include/complex_number.h"
 
-TEST(Pinaev_Danil_ComplexNumberTest, can_create_complex_numbers) {
+TEST(Pinaev_Danil_ComplexNumberTest, Ñan_Ñreate_Ñomplex_Òumbers) {
     // Assert
     ASSERT_NO_THROW(ComplexNumber a(1.0, 1.0));
 }
 
 
-TEST(Pinaev_Danil_ComplexNumberTest, can_add_complex_numbers) {
+TEST(Pinaev_Danil_ComplexNumberTest, Can_Add_Complex_Numbers) {
     // Arrange
     ComplexNumber a(1, -2);
     ComplexNumber b(3, -2);
@@ -20,7 +20,7 @@ TEST(Pinaev_Danil_ComplexNumberTest, can_add_complex_numbers) {
 }
 
 
-TEST(Pinaev_Danil_ComplexNumberTest, can_subtract_complex_numbers) {
+TEST(Pinaev_Danil_ComplexNumberTest, Can_Subtract_Complex_Numbers) {
     // Arrange
     ComplexNumber a(1, -2);
     ComplexNumber b(3, -2);
@@ -30,7 +30,7 @@ TEST(Pinaev_Danil_ComplexNumberTest, can_subtract_complex_numbers) {
 }
 
 
-TEST(Pinaev_Danil_ComplexNumberTest, can_multiple_complex_numbers) {
+TEST(Pinaev_Danil_ComplexNumberTest, Can_Multiple_Complex_Numbers) {
     // Arrange
     ComplexNumber a(1, -2);
     ComplexNumber b(3, -2);
@@ -39,11 +39,65 @@ TEST(Pinaev_Danil_ComplexNumberTest, can_multiple_complex_numbers) {
     ASSERT_EQ(a * b, ComplexNumber(-1, -8));
 }
 
-TEST(Pinaev_Danil_ComplexNumberTest, can_not_division_on_zero_complex_number) {
+
+TEST(Pinaev_Danil_ComplexNumberTest, Can_Not_Division_On_Zero_Complex_Number) {
     // Arrange
     ComplexNumber a(1, -2);
     ComplexNumber b(0, 0);
 
     // Assert
     ASSERT_ANY_THROW(a / b);
+}
+
+
+TEST(Pinaev_Danil_ComplexNumberTest, Have_A_Defauld_Value) {
+	// Arrange
+	ComplexNumber a;
+	ComplexNumber b;
+
+	// Assert
+	ASSERT_EQ(a, b);
+}
+
+
+TEST(Pinaev_Danil_ComplexNumberTest, Defauld_Value_Is_Zero_Value) {
+	// Arrange
+	ComplexNumber a(0, 0);
+	ComplexNumber b;
+
+	// Assert
+	ASSERT_EQ(a, b);
+}
+
+
+TEST(Pinaev_Danil_ComplexNumberTest, Can_Create_Arr_Complex_Numbers) {
+	// Assert
+	ASSERT_NO_THROW(ComplexNumber A[5]);
+}
+
+
+TEST(Pinaev_Danil_ComplexNumberTest, Can_Add_To_Arr) {
+	// Arrange
+	ComplexNumber a(1, -2);
+	ComplexNumber A[5];
+
+	// Assert
+	ASSERT_NO_THROW(A[0] = a);
+}
+
+
+TEST(Pinaev_Danil_ComplexNumberTest, Some_Weird_Acts_With_Arr) {
+	// Arrange
+	ComplexNumber a, b;
+	ComplexNumber A[4];
+	A[0] = ComplexNumber(3, 4);
+	A[1] = ComplexNumber(-3, -4);
+	A[2] = ComplexNumber(5, 6);
+	A[3] = ComplexNumber(7, 8);
+	
+	// Act
+	b = ((A[0] + A[1]) / (A[2] * A[3]));
+
+	// Assert
+	ASSERT_EQ(a, b);
 }
