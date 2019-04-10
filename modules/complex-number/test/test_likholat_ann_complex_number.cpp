9c7d0eb595c85c2a1834ae066967fe078934ebd1
likholat <aniala201398@gmail.com>
2019-04-10 21:24:33 +0300

    detab

--- modules/complex-number/test/test_likholat_ann_complex_number.cpp
+++ modules/complex-number/test/test_likholat_ann_complex_number.cpp
@@ -5,53 +5,53 @@
 #include "include/complex_number.h"
 
 TEST(Likholat_Ann_ComplexNumberTest, Can_Create_Copy) {
-	// Arrange
-	ComplexNumber z(2.3, 1.4);
+    // Arrange
+    ComplexNumber z(2.3, 1.4);
 
-	// Act
-	ComplexNumber z_1(z);
+    // Act
+    ComplexNumber z_1(z);
 
-	// Assert
-	EXPECT_EQ(z, z_1);
+    // Assert
+    EXPECT_EQ(z, z_1);
 }
 
 TEST(Likholat_Ann_ComplexNumberTest, Addition_Associativy) {
-	// Arrange
-	ComplexNumber z(2.0, 1.4);
-	ComplexNumber z_1(4.6, 3.0);
-	ComplexNumber z_2(7.3, 1.2);
+    // Arrange
+    ComplexNumber z(2.0, 1.4);
+    ComplexNumber z_1(4.6, 3.0);
+    ComplexNumber z_2(7.3, 1.2);
 
-	// Act
-	ComplexNumber res = (z + z_1) + z_2;
-	ComplexNumber res_1 = z + (z_1 + z_2);
+    // Act
+    ComplexNumber res = (z + z_1) + z_2;
+    ComplexNumber res_1 = z + (z_1 + z_2);
 
-	// Assert
-	EXPECT_DOUBLE_EQ(res.getRe(), res_1.getRe());
-	EXPECT_DOUBLE_EQ(res.getIm(), res_1.getIm());
+    // Assert
+    EXPECT_DOUBLE_EQ(res.getRe(), res_1.getRe());
+    EXPECT_DOUBLE_EQ(res.getIm(), res_1.getIm());
 }
 
 TEST(Likholat_Ann_ComplexNumberTest, Can_Multiply_By_Zero) {
-	// Arrange
-	ComplexNumber z(2.0, 1.0);
-	ComplexNumber zero(0.0, 0.0);
+    // Arrange
+    ComplexNumber z(2.0, 1.0);
+    ComplexNumber zero(0.0, 0.0);
 
-	// Act
-	ComplexNumber res = z * zero;
+    // Act
+    ComplexNumber res = z * zero;
 
-	// Assert
-	EXPECT_EQ(res, zero);
+    // Assert
+    EXPECT_EQ(res, zero);
 }
 
 
 TEST(Likholat_Ann_ComplexNumberTest, Can_Multiply_By_Itself) {
-	// Arrange
-	ComplexNumber z(2.3, 1.2);
+    // Arrange
+    ComplexNumber z(2.3, 1.2);
 
-	// Act
-	ComplexNumber res = z * z;
+    // Act
+    ComplexNumber res = z * z;
 
-	// Assert
-	ComplexNumber expected_res(3.85, 5.52);
-	EXPECT_DOUBLE_EQ(expected_res.getRe(), res.getRe());
-	EXPECT_TRUE(expected_res.getIm(), res.getIm());
+    // Assert
+    ComplexNumber expected_res(3.85, 5.52);
+    EXPECT_DOUBLE_EQ(expected_res.getRe(), res.getRe());
+    EXPECT_TRUE(expected_res.getIm(), res.getIm());
 }
