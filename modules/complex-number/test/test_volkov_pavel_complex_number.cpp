419642a160b414ab96d8eba74edb69612ba2639a
Snewar79 <himera5x@yandex.ru>
2019-04-10 21:11:28 +0300

    Need mooore codestyle fix

--- modules/complex-number/test/test_volkov_pavel_complex_number.cpp
+++ modules/complex-number/test/test_volkov_pavel_complex_number.cpp
@@ -15,14 +15,14 @@ TEST(Volkov_Pavel_ComplexNumberTest, Copy_Constructor_Is_Not_Deep) {
 
     // Act
     ComplexNumber z1(re, im);
-    
+
     ComplexNumber z2(z1);
 
     z2.setRe(re_changed);                      
-    z2.setIm(im_changed);  
+    z2.setIm(im_changed);
 
     // Assert
-    EXPECT_EQ(re, z1.getRe());                 
+    EXPECT_EQ(re, z1.getRe());
     EXPECT_EQ(im, z1.getIm());
 
     EXPECT_EQ(re_changed, z2.getRe());
@@ -37,27 +37,25 @@ TEST(Volkov_Pavel_ComplexNumberTest, Imagine_One_Sqr_Equal_Negative_One) {
     double im = 1.0;
 
     // Act
-    ComplexNumber z1(re, im);         
+    ComplexNumber z1(re, im);
     ComplexNumber z2;
 
-    z2 = z1 * z1;                
+    z2 = z1 * z1;
 
     // Assert
     EXPECT_EQ(-1.0, z2.getRe());
     EXPECT_EQ(0.0, z2.getIm());
-    
+
 }
 
 TEST(Volkov_Pavel_ComplexNumberTest, Assignment_Operator_Return_Correct_Link) {
-    
+
     // Arrange
     ComplexNumber z1;
     ComplexNumber z2(2.0, 3.0);
     ComplexNumber *returned_link;
 
     // Act
-
-    // Get the link after Assignment
     returned_link = &(z1 = z2);
 
 
@@ -65,10 +63,4 @@ TEST(Volkov_Pavel_ComplexNumberTest, Assignment_Operator_Return_Correct_Link) {
     EXPECT_EQ(returned_link, &z1);
 
 }
-
-
-
-
-
-
 
