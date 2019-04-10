6e3d0cc6eacd77af03ea620f185bccb9cda2d5a8
likholat <aniala201398@gmail.com>
2019-04-10 21:50:31 +0300

    Updated tests

--- modules/complex-number/test/test_likholat_ann_complex_number.cpp
+++ modules/complex-number/test/test_likholat_ann_complex_number.cpp
@@ -53,5 +53,5 @@ TEST(Likholat_Ann_ComplexNumberTest, Can_Multiply_By_Itself) {
     // Assert
     ComplexNumber expected_res(3.85, 5.52);
     EXPECT_DOUBLE_EQ(expected_res.getRe(), res.getRe());
-	EXPECT_DOUBLE_EQ(expected_res.getIm(), res.getIm());
+    EXPECT_DOUBLE_EQ(expected_res.getIm(), res.getIm());
 }
