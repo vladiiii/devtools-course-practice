73b25dc72be109b6ab2268175bab24e0b9dfe634
Gerilda <anagerilda@mail.ru>
2019-04-11 22:18:36 +0300

    Updated tests

--- modules/complex-number/test/test_trubina_anastasia_complex_number.cpp
+++ modules/complex-number/test/test_trubina_anastasia_complex_number.cpp
@@ -2,16 +2,16 @@
 
 #include <gtest/gtest.h>
 
-#include "include/complex_number.h"
-
 #include <cfloat>
+
+#include "include/complex_number.h"
 
 TEST(Trubina_Anastasia_ComplexNumberTest, Property_Of_The_Imaginary_Unit) {
 	// Arrange
 	ComplexNumber i(0, 1.0);
 	ComplexNumber a(-1.0, 0);
 	bool res;
-
+	
 	//Act
 	res = a == i * i;
 
@@ -19,16 +19,16 @@ TEST(Trubina_Anastasia_ComplexNumberTest, Property_Of_The_Imaginary_Unit) {
 	ASSERT_TRUE(res);
 }
 
-TEST(Trubina_Anastasia_ComplexNumberTest, Are_Additive_Inverse_Complex_Numbers) {
+TEST(Trubina_Anastasia_ComplexNumberTest, Is_Additive_Inverse_Complex_Number) {
 	//Arrange
 	ComplexNumber dir(1.0, 1.0);
 	ComplexNumber inv(-1.0, -1.0);
 	ComplexNumber res(0.0, 0.0);
 	bool result;
-
+	
 	//Act
 	result = res == dir + inv;
-
+	
 	//Assert
 	ASSERT_TRUE(result);
 }
@@ -46,27 +46,29 @@ TEST(Trubina_Anastasia_ComplexNumberTest, Correctness_Of_Operation_Set) {
 	EXPECT_FLOAT_EQ(4.7, res.getIm());
 }
 
-TEST(Trubina_Anastasia_ComplexNumberTest, Are_Reciprocal_Complex_Number) {
+TEST(Trubina_Anastasia_ComplexNumberTest, Is_Reciprocal_Complex_Number) {
 	//Arrange
 	ComplexNumber dir(3.0, 4.0);
-
+	
 	//Act
-	ComplexNumber rec(dir.getRe() / (dir.getRe() * dir.getRe() + dir.getIm() * dir.getIm()),
-		              dir.getIm() / (dir.getRe() * dir.getRe() + dir.getIm() * dir.getIm()));
-
+	ComplexNumber rec(dir.getRe() / (dir.getRe() * dir.getRe() +
+		dir.getIm() * dir.getIm()), dir.getIm() / (dir.getRe() *
+		dir.getRe() + dir.getIm() * dir.getIm()));
+	
 	//Assert
 	EXPECT_FLOAT_EQ(0.12, rec.getRe());
 	EXPECT_FLOAT_EQ(0.16, rec.getIm());
 }
 
-TEST(Trubina_Anastasia_ComplexNumberTest, Infinity_Is_Not_Reciprocal_Of_Complex_Null) {
+TEST(Trubina_Anastasia_ComplexNumberTest, Infinity_Not_Reciprocal_Null) {
 	//Arrange
-	ComplexNumber dir(0.0, 0.0);
-
+	ComplexNumber dir(0, 0);
+	
 	//Act
-	ComplexNumber rec(dir.getRe() / (dir.getRe() * dir.getRe() + dir.getIm() * dir.getIm()),
-		dir.getIm() / (dir.getRe() * dir.getRe() + dir.getIm() * dir.getIm()));
-
+	ComplexNumber rec(dir.getRe() / (dir.getRe() * dir.getRe() +
+		dir.getIm() * dir.getIm()), dir.getIm() / (dir.getRe() *
+		dir.getRe() + dir.getIm() * dir.getIm()));
+	
 	//Assert
 	EXPECT_NE(DBL_MAX, rec.getRe());
 	EXPECT_NE(DBL_MAX, rec.getIm());
