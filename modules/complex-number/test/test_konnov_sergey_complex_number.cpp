ae92e3a256f68ae4decb2e905004d06799492b14
Сергей Коннов <sergeyzorro007@gmail.com>
2019-04-11 07:56:44 +0300

    Correct year and remove uncorrect test

--- modules/complex-number/test/test_konnov_sergey_complex_number.cpp
+++ modules/complex-number/test/test_konnov_sergey_complex_number.cpp
@@ -1,21 +1,8 @@
-// Copyright 2017 Konnov Sergey
+// Copyright 2019 Konnov Sergey
 
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
-
-TEST(Konnov_Sergey_ComplexNumberTest, Can_Multiply) {
-    // Arrange
-    ComplexNumber z1(3.0, 4.0);
-    ComplexNumber z2(4.0, 5.0);
-
-    // Act
-    ComplexNumber z = z1*z2;
-
-    // Assert
-    ComplexNumber expected_z(-8.0, 31.0);
-    EXPECT_EQ(expected_z, z);
-}
 
 TEST(Konnov_Sergey_ComplexNumberTest, Can_Sum_Multiple_Times) {
     // Arrange
