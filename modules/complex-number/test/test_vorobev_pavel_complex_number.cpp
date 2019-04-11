ca7b4659a642f3bda15aeb05f9540e5496a1d2cc
Pavel Vorobev <sayhello98123@gmail.com>
2019-04-11 09:54:44 +0300

    fixed mistakes

--- modules/complex-number/test/test_vorobev_pavel_complex_number.cpp
+++ modules/complex-number/test/test_vorobev_pavel_complex_number.cpp
@@ -4,78 +4,54 @@
 
 #include "include/complex_number.h"
 
-TEST(Vorobev_Pavel_ComplexNumberTest, Can_Create_Zero) {
+TEST(Vorobev_Pavel_ComplexNumberTest, Can_Check_Equality) {
     // Arrange
     double re = 0.0;
     double im = 0.0;
 
     // Act
     ComplexNumber z(re, im);
+    ComplexNumber y(re, im);
 
     // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ(true, z == y);
 }
 
 TEST(Vorobev_Pavel_ComplexNumberTest, Can_Add_Two_Numbers) {
-    // Arrange
-    double re = 1.0;
-    double im = 2.0;
-
-    double re1 = 3.0;
-    double im1 = 5.0;
     // Act
-    ComplexNumber x(re, im);
-    ComplexNumber y(re1, im1);
+    ComplexNumber x(1.0, 2.0);
+    ComplexNumber y(2.0, 3.0);
     ComplexNumber z = x + y;
 
     // Assert
-    EXPECT_EQ(re + re1, z.getRe());
-    EXPECT_EQ(im + im1, z.getIm());
+    EXPECT_EQ(3.0, z.getRe());
+    EXPECT_EQ(5.0, z.getIm());
 }
 
 TEST(Vorobev_Pavel_ComplexNumberTest, Can_Substract_Two_Numbers) {
-    // Arrange
-    double re = 11.0;
-    double im = 12.0;
-
-    double re1 = 3.0;
-    double im1 = 5.0;
     // Act
-    ComplexNumber x(re, im);
-    ComplexNumber y(re1, im1);
+    ComplexNumber x(11.0, 12.0);
+    ComplexNumber y(3.0, 5.0);
     ComplexNumber z = x - y;
 
     // Assert
-    EXPECT_EQ(re - re1, z.getRe());
-    EXPECT_EQ(im - im1, z.getIm());
+    EXPECT_EQ(8.0, z.getRe());
+    EXPECT_EQ(7.0, z.getIm());
 }
 
 TEST(Vorobev_Pavel_ComplexNumberTest, Can_Check_Inequality) {
-    // Arrange
-    double re = 1.0;
-    double im = 12.0;
-
-    double re1 = 4.0;
-    double im1 = 7.0;
     // Act
-    ComplexNumber x(re, im);
-    ComplexNumber y(re1, im1);
+    ComplexNumber x(1.0, 12.0);
+    ComplexNumber y(4.0, 7.0);
 
     // Assert
     EXPECT_EQ(true, x != y);
 }
 
 TEST(Vorobev_Pavel_ComplexNumberTest, Can_Multiply_Two_Numbers) {
-    // Arrange
-    double re = 1.0;
-    double im = 1.0;
-
-    double re1 = 3.0;
-    double im1 = 5.0;
     // Act
-    ComplexNumber x(re, im);
-    ComplexNumber y(re1, im1);
+    ComplexNumber x(1.0, 1.0);
+    ComplexNumber y(3.0, 5.0);
     ComplexNumber z = x * y;
 
     // Assert
