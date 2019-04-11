a7eae9f9b316f4bf950f38cf47e8cb2f6c72a8eb
ishkerin <ishkerin@mail.ru>
2019-04-11 21:11:58 +0300

    Added Correct_Setter_And_Getter test

--- modules/complex-number/test/test_shkerin_igor_complex_number.cpp
+++ modules/complex-number/test/test_shkerin_igor_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Shkerin_Igor_ComplexNumberTest, Can_Create_Negative_complex) {
+TEST(Shkerin_Igor_ComplexNumberTest, Can_Create_Negative_Complex) {
     // Arrange
     double re = -3.0;
     double im = -7.0;
@@ -15,4 +15,19 @@ TEST(Shkerin_Igor_ComplexNumberTest, Can_Create_Negative_complex) {
     // Assert
     EXPECT_EQ(re, z.getRe());
     EXPECT_EQ(im, z.getIm());
+}
+
+TEST(Shkerin_Igor_ComplexNumberTest, Correct_Setter_And_Getter) {
+    // Arrange
+    double re1 = 1.0;
+    double im1 = 2.0;
+
+    // Act
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2;
+    z2.setRe(z1.getRe());
+    z2.setIm(z1.getIm());
+
+    // Assert
+    EXPECT_EQ(z1, z2);
 }
