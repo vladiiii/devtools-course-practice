26b454acb897e4c6d34fbfb863d8ef1c25cc9397
ishkerin <ishkerin@mail.ru>
2019-04-11 19:12:31 +0300

    Added unique tests.

--- modules/complex-number/test/test_shkerin_igor_complex_number.cpp
+++ modules/complex-number/test/test_shkerin_igor_complex_number.cpp
@@ -2,13 +2,12 @@
 
 #include <gtest/gtest.h>
 
-#include <string>
 #include "include/complex_number.h"
 
-TEST(Shkerin_Igor_ComplexNumberTest, Can_Create_Zero) {
+TEST(Shkerin_Igor_ComplexNumberTest, Can_Create_Negative_complex) {
     // Arrange
-    double re = 0.0;
-    double im = 0.0;
+    double re = -3.0;
+    double im = -7.0;
 
     // Act
     ComplexNumber z(re, im);
@@ -18,21 +17,19 @@ TEST(Shkerin_Igor_ComplexNumberTest, Can_Create_Zero) {
     EXPECT_EQ(im, z.getIm());
 }
 
-TEST(Shkerin_Igor_ComplexNumberTest, Can_Sum_Two_Complex_Numbers) {
+TEST(Shkerin_Igor_ComplexNumberTest, Correct_Setter_And_Getter) {
     // Arrange
     double re1 = 1.0;
     double im1 = 2.0;
-    double re2 = 4.0;
-    double im2 = 5.0;
 
     // Act
     ComplexNumber z1(re1, im1);
-    ComplexNumber z2(re2, im2);
-    ComplexNumber z3 = z1 + z2;
-    ComplexNumber expected_z3(5.0, 7.0);
+    ComplexNumber z2;
+	z2.setRe(z1.getRe());
+	z2.setIm(z1.getIm());
 
     // Assert
-    EXPECT_EQ(z3, expected_z3);
+    EXPECT_EQ(z1, z2);
 }
 
 TEST(Shkerin_Igor_ComplexNumberTest, Can_Multiplication_Complex_By_Zero) {
@@ -48,13 +45,13 @@ TEST(Shkerin_Igor_ComplexNumberTest, Can_Multiplication_Complex_By_Zero) {
     EXPECT_EQ(z3, expected_z3);
 }
 
-TEST(Shkerin_Igor_ComplexNumberTest, Throw_Multiplication_Complex_By_Zero) {
+TEST(Shkerin_Igor_ComplexNumberTest, Multiplication_By_One_Give_The_Same) {
     // Arrange
     ComplexNumber z1(27.0, 11.0);
-    ComplexNumber zero(0.0, 0.0);
+    ComplexNumber z2(1.0, 0.0);
 
     // Act & Assert
-    EXPECT_THROW(z1/zero, std::string);
+    EXPECT_EQ(z1*z2, z1);
 }
 
 TEST(Shkerin_Igor_ComplexNumberTest, Different_Equal_Initialize_Is_Equal) {
