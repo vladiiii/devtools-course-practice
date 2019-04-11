f42c012f04ca24a16a101306a50e25ebed8f6cee
kasmazyuk <kasmazyuk2010@yandex.ru>
2019-04-11 15:44:01 +0300

    Update tests

--- modules/complex-number/test/test_Kasmazyuk_Nikita_complex_number.cpp
+++ modules/complex-number/test/test_Kasmazyuk_Nikita_complex_number.cpp
@@ -4,46 +4,55 @@
 
 #include "include/complex_number.h"
 
-TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Create_Zero) {
+TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Create_Complex_Number {
 // Arrange
-    double re = 0.0;
-    double im = 0.0;
+    double re = 2.0;
+    double im = 3.0;
 // Act
-    ComplexNumber z(re, im);
+    ComplexNumber e(re, im);
 // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ(re, e.getRe());
+    EXPECT_EQ(im, e.getIm());
 }
 
 TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Sum_Complex_Number) {
 // Arrange
-    ComplexNumber z1(15.0, 15.0);
-    ComplexNumber z2(5.0, 10.0);
-    ComplexNumber z;
+    ComplexNumber q(15.0, 15.0);
+    ComplexNumber w(5.0, 10.0);
+    ComplexNumber e;
 // Act
-    z = z1 + z2;
+    e = q + w;
 // Assert
-    EXPECT_EQ(20.0, z.getRe());
-    EXPECT_EQ(25.0, z.getIm());
+    EXPECT_EQ(20.0, e.getRe());
+    EXPECT_EQ(25.0, e.getIm());
 }
 
 TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Multiplication_Complex_Number) {
 // Arrange
-    ComplexNumber z1(15.0, 15.0);
-    ComplexNumber z2(5.0, 10.0);
-    ComplexNumber z;
+    ComplexNumber q(15.0, 15.0);
+    ComplexNumber w(5.0, 10.0);
+    ComplexNumber e;
 // Act
-    z = z1 * z2;
+    e = q * w;
 // Assert
-    EXPECT_EQ(-75.0, z.getRe());
-    EXPECT_EQ(225.0, z.getIm());
+    EXPECT_EQ(-75.0, e.getRe());
+    EXPECT_EQ(225.0, e.getIm());
 }
 
 TEST(Kasmazyuk_Nikita_ComplexNumberTest, Equal_Numbers_Are_Equal) {
 // Arrange
-    ComplexNumber z1(15.0, 15.0);
-    ComplexNumber z2(15.0, 15.0);
+    ComplexNumber q(15.0, 15.0);
+    ComplexNumber w(15.0, 15.0);
 // Act & Assert
-    EXPECT_EQ(z1.getRe(), z2.getRe());
-    EXPECT_EQ(z1.getIm(), z2.getIm());
+    EXPECT_EQ(q.getRe(), w.getRe());
+    EXPECT_EQ(q.getIm(), w.getIm());
+}
+
+TEST(Kasmazyuk_Nikita_ComplexNumberTest, Not_Equal_Numbers_Not_Equal) {
+// Act
+    ComplexNumber q(1.0, 2.0);
+    ComplexNumber w(15.0, 15.0);
+
+//  Act & Assert
+    EXPECT_EQ(true, q != w);
 }
