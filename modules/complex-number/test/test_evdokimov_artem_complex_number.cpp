691f168cef5408941d0b6464a800e9ecca5be7a9
black8cherry <art8evd@gmail.com>
2019-04-11 23:38:47 +0300

    fix travis num3

--- modules/complex-number/test/test_evdokimov_artem_complex_number.cpp
+++ modules/complex-number/test/test_evdokimov_artem_complex_number.cpp
@@ -1,56 +1,69 @@
-// Copyright 2019 Evdokimov Artem 
+// Copyright 2019 Evdokimov Artem
 
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
 
 TEST(Evdokimov_Artem_ComplexNumberTest, Copy_Has_Its_Own_Memory) {
-    // Arrange 
+    // Arrange
     double re = 1.1;
     double im = 1.1;
 
-    // Act 
+    // Act
     ComplexNumber a(re , im);
-    ComplexNumber b(a);Ы
+    ComplexNumber b(a);
 
-    // Assert 
+    // Assert
     EXPECT_NE(&a , &b);
 }
 
-TEST(Evdokimov_Artem_ComplexNumberTest, Self_�omparison) {
-    // Arrange 
+TEST(Evdokimov_Artem_ComplexNumberTest, Can_Assign) {
+    // Arrange
     double re = 1.1;
     double im = 1.1;
 
-    // Act 
+    // Act
+    ComplexNumber a(re , im);
+    ComplexNumber b(1.1 , 1.1);
+
+    // Assert
+    EXPECT_EQ(a = b);
+}
+TEST(Evdokimov_Artem_ComplexNumberTest, Self_Assign) {
+    // Arrange
+    double re = 1.1;
+    double im = 1.1;
+
+    // Act
     ComplexNumber a(re , im);
 
-    // Assert 
+    // Assert
     ASSERT_NO_THROW(a = a);
 }
 
 TEST(Evdokimov_Artem_ComplexNumberTest, Can_Create_Object_Without_Arguments) {
-    // Arrange 
+    // Arrange
     double re = 0.0;
     double im = 0.0;
 
-    //Act 
+    //Act
     ComplexNumber a(re , im);
     ComplexNumber b;
 
-    //Assert 
+    //Assert
     ASSERT_NO_THROW(a , b);
 }
 
 TEST(Evdokimov_Artem_ComplexNumberTest, Can_Multiply_By_Zero) {
-    // Arrange 
+    // Arrange
     double re = 1.1;
     double im = 1.1;
 
-    // Act 
+    // Act
     ComplexNumber a(re , im);
     ComplexNumber b(0.0 , 0.0);
 
-    // Assert 
+    // Assert
     EXPECT_EQ(b , a * b);
 }
+    
