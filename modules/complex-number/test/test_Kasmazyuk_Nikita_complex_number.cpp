6ce766b57027e937ab25ebdcace84b1409def5cb
kasmazyuk <kasmazyuk2010@yandex.ru>
2019-04-11 15:06:56 +0300

    Update tests

--- modules/complex-number/test/test_Kasmazyuk_Nikita_complex_number.cpp
+++ modules/complex-number/test/test_Kasmazyuk_Nikita_complex_number.cpp
@@ -14,3 +14,48 @@ TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Create_Zero) {
     EXPECT_EQ(re, z.getRe());
     EXPECT_EQ(im, z.getIm());
 }
+
+TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Sum_Complex_Number) {
+// Arrange
+    ComplexNumber z1(15.0, 15.0);
+    ComplexNumber z2(5.0, 10.0);
+    ComplexNumber z;
+// Act
+    z = z1 + z2;
+// Assert
+    EXPECT_EQ(20.0, z.getRe());
+    EXPECT_EQ(25.0, z.getIm());
+}
+
+TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Sum_Complex_Number) {
+// Arrange
+    ComplexNumber z1(15.0, 15.0);
+    ComplexNumber z2(5.0, 10.0);
+    ComplexNumber z;
+// Act
+    z = z1 - z2
+// Assert
+    EXPECT_EQ(10.0, z.getRe());
+    EXPECT_EQ(5.0, z.getIm());
+}
+
+TEST(Kasmazyuk_Nikita_ComplexNumberTest, Can_Multiplication_Complex_Number) {
+// Arrange
+    ComplexNumber z1(15.0, 15.0);
+    ComplexNumber z2(5.0, 10.0);
+    ComplexNumber z;
+// Act
+    z = z1 * z2;
+// Assert
+    EXPECT_EQ(75.0, z.getRe());
+    EXPECT_EQ(150.0, z.getIm());
+}
+
+TEST(Kasmazyuk_Nikita_ComplexNumberTest, Equal_Numbers_Are_Equal) {
+// Arrange
+    ComplexNumber z1(15.0, 15.0);
+    ComplexNumber z2(15.0, 15.0);
+// Act & Assert
+    EXPECT_EQ(z1.getRe(), z2.getRe());
+    EXPECT_EQ(z1.getIm(), z2.getIm());
+}
