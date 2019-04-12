b9892d6a1e7414e33594ea14ac5bd763b5838a45
Enreyz <enreys@mail.ru>
2019-04-12 23:57:36 +0300

    Updated tests

--- modules/complex-number/test/test_druzhinin_alexei_complex_number.cpp
+++ modules/complex-number/test/test_druzhinin_alexei_complex_number.cpp
@@ -4,77 +4,43 @@
 
 #include "include/complex_number.h"
 
-TEST(Druzhinin_Alexei_ComplexNumberTest, Can_add_complex_numbers) {
+#include <math.h>
+
+TEST(Druzhinin_Alexei_ComplexNumberTest, Check_All_Operations_With_Priority) {
     // Arrange
-    ComplexNumber z1(3.2, 6.8);
-    ComplexNumber z2(6.8, 3.2);
+    ComplexNumber z1(1.0, 1.0);
+    ComplexNumber z2(2.0, 2.0);
+	ComplexNumber z3(4.0, 0.0);
+	ComplexNumber z4(-1.0, -1.0);
+	ComplexNumber z5(5.0, 5.0);
 
     // Act
-    ComplexNumber z = z1 + z2;
-
-    // Assert
-    EXPECT_EQ(10.0, z.getRe());
-    EXPECT_EQ(10.0, z.getIm());
-}
-
-TEST(Druzhinin_Alexei_ComplexNumberTest, Can_sub_complex_numbers) {
-    // Arrange
-    ComplexNumber z1(6.2, 3.3);
-    ComplexNumber z2(1.2, -1.7);
-
-    // Act
-    ComplexNumber z = z1 - z2;
-
-    // Assert
-    EXPECT_EQ(5.0, z.getRe());
-    EXPECT_EQ(5.0, z.getIm());
-}
-
-TEST(Druzhinin_Alexei_ComplexNumberTest, Can_multiply_complex_numbers) {
-    // Arrange
-    ComplexNumber z1(5.0, 1.0);
-    ComplexNumber z2(1.0, 5.0);
-
-    // Act
-    ComplexNumber z = z1 * z2;
-
-    // Assert
-    EXPECT_EQ(0.0, z.getRe());
-    EXPECT_EQ(26.0, z.getIm());
-}
-
-TEST(Druzhinin_Alexei_ComplexNumberTest, Can_division_complex_numbers) {
-    // Arrange
-    ComplexNumber z1(5.0, 1.0);
-    ComplexNumber z2(z1);
-
-    // Act
-    ComplexNumber z = z1 / z2;
+    ComplexNumber z = (z1 * z2 + z3 - z4) / z5;
 
     // Assert
     EXPECT_EQ(1.0, z.getRe());
     EXPECT_EQ(0.0, z.getIm());
 }
 
-TEST(Druzhinin_Alexei_ComplexNumberTest, Can_compare_eq_complex_numbers) {
+TEST(Druzhinin_Alexei_ComplexNumberTest, Pow_Of_Imaginary_Unit) {
     // Arrange
-    ComplexNumber z1(1.0, 1.0);
+    ComplexNumber z1(0, 1.0);
+	bool res;
+	
+    // Act
+    res = z1 == z1*z1*z1*z1*z1;
+
+    // Assert
+    ASSERT_TRUE(res);
+}
+
+TEST(Druzhinin_Alexei_ComplexNumberTest, Copy_Of_Complex_Number) {
+    // Arrange
+    ComplexNumber z1(5.0, 5.0);
 
     // Act
-    ComplexNumber z2 = z1;
+    ComplexNumber z2(z1);
 
     // Assert
     EXPECT_EQ(z1, z2);
-}
-
-TEST(Druzhinin_Alexei_ComplexNumberTest, Can_compare_neq_complex_numbers) {
-    // Arrange
-    ComplexNumber z1(1.0, 1.0);
-
-    // Act
-    ComplexNumber z2 = z1;
-    z2.setRe(2.0);
-
-    // Assert
-    EXPECT_NE(z1, z2);
 }
