c73ea6a0342985f6cb8a160f45eaf599173727b0
lesya89 <zolota98@mail.ru>
2019-04-12 14:14:46 +0300

    add first test for first lab

--- modules/complex-number/test/test_zolotareva_olesya_complex_number.cpp
+++ modules/complex-number/test/test_zolotareva_olesya_complex_number.cpp
@@ -22,35 +22,3 @@ TEST(Zolotareva_Olesya_NumberComplex_test, Can_distribute_one) {
 	EXPECT_FLOAT_EQ(-53.0, res.getRe());
 	EXPECT_FLOAT_EQ(76.0, res.getIm());
 }
-
-TEST(Zolotareva_Olesya_NumberComplex_test, Can_distribute_two) {
-	//arrange
-	ComplexNumber num1(2.0, 3.0);
-	ComplexNumber num2(1.0, 4.0);
-	ComplexNumber num3(5.0, 6.0);
-
-	//act
-	ComplexNumber res = (num1 * num2) + (num1 * num3);
-
-	//assert
-	EXPECT_FLOAT_EQ(-18.0, res.getRe());
-	EXPECT_FLOAT_EQ(34.0, res.getIm());
-}
-
-TEST(Zolotareva_Olesya_NumberComplex_test, Can_complex_logarifm) {
-	//arrange
-	double Re = 3.0;
-	double Im = 4.0;
-
-	//act
-	double R = sqrt(pow(Re, 2) + pow(Im, 2));
-	double Phi = Re / Im;
-
-	Re = log10(R);
-	Im = Phi + 2 * M_PI;
-
-	ComplexNumber res;
-	//assert
-	EXPECT_FLOAT_EQ(Re, res.getRe());
-	EXPECT_FLOAT_EQ(Im, res.getIm());
-}
