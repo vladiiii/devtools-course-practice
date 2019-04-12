d7d59af3c5a79200f60c57501b06fe2f689c39f8
J-win <j.vdowin@yandex.ru>
2019-04-12 11:47:55 +0300

    delete if

--- modules/complex-number/test/test_vdovin_eugene_complex_number.cpp
+++ modules/complex-number/test/test_vdovin_eugene_complex_number.cpp
@@ -5,41 +5,24 @@
 #include "include/complex_number.h"
 
 TEST(Vdovin_Eugene_ComplexNumberTest, Can_Create_Zero_Two_Way) {
-    bool f = false;
-
     ComplexNumber cn(0.0, 0.0);
     ComplexNumber cn2;
 
-    if (cn2 == cn) {
-        f = true;
-    } else {
-        f = false;
-    }
-
-    EXPECT_EQ(true, f);
+    EXPECT_EQ(cn, cn2);
 }
 
 TEST(Vdovin_Eugene_ComplexNumberTest, Work_Triple_Assignment) {
-    bool f = false;
-
     ComplexNumber cn1(3.0, 4.0);
     ComplexNumber cn2(-5.0, 6.3);
     ComplexNumber cn3(-8.0, -9.3);
 
     cn1 = cn2 = cn3;
 
-    if ((cn1 == cn3) && (cn2 == cn3)) {
-        f = true;
-    } else {
-        f = false;
-    }
-
-    EXPECT_EQ(true, f);
+    EXPECT_EQ(cn3, cn1);
+    EXPECT_EQ(cn3, cn2);
 }
 
 TEST(Vdovin_Eugene_ComplexNumberTest, Mult_And_Div_Compensation) {
-    bool f = false;
-
     // Only at such coefficients, otherwise the computational error is large
 
     ComplexNumber cn1(4.0, 4.0);
@@ -48,11 +31,5 @@ TEST(Vdovin_Eugene_ComplexNumberTest, Mult_And_Div_Compensation) {
 
     cn3 = (cn1 * cn2) / cn1;
 
-    if (cn2 == cn3) {
-        f = true;
-    } else {
-        f = false;
-    }
-
-    EXPECT_EQ(true, f);
+    EXPECT_EQ(cn2, cn3);
 }
