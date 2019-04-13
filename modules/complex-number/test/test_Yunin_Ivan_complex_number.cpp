7ee181c892899a056537a7f998ce75b1197e876b
IvanYunin <79103889939@yandex.ru>
2019-04-13 19:28:57 +0300

    fix bugs

--- modules/complex-number/test/test_Yunin_Ivan_complex_number.cpp
+++ modules/complex-number/test/test_Yunin_Ivan_complex_number.cpp
@@ -6,32 +6,24 @@
 
 TEST(Yunin_Ivan_ComplexNumberTest, Priority_Of_Multiplication_Operation) {
     // Arrange
-
     ComplexNumber z1(1.0, 1.0);
     ComplexNumber z2(2.0, 2.0);
     ComplexNumber z3(3.0, 3.0);
     ComplexNumber ans(1.0, 13.0);
-
     // Act
-
     ComplexNumber res = z1 + z2 * z3;
+    ComplexNumber check = z1 + (z2 * z3);
     // Assert
-
     EXPECT_EQ(ans, res);
 }
 
 TEST(Yunin_Ivan_ComplexNumberTest, Rize_To_Third_Degree ) {
     // Arrange
-
     ComplexNumber z(7, 3);
     ComplexNumber ans(154, 414);
-
     // Act
-
     ComplexNumber res = z * z * z;
-
     // Assert
-
     EXPECT_EQ(ans, res);
 }
 
@@ -40,10 +32,8 @@ TEST(Yunin_Ivan_ComplexNumberTest, DISABLED_Direct_And_Inverse_Operation) {
     ComplexNumber z1(1.0, 2.0);
     ComplexNumber z2(5.0, 3.0);
     ComplexNumber ans = z1;
-
     // Act
     ComplexNumber res = (z1 / z2) * z2;
-
     // Assert
     EXPECT_EQ(ans, res);
 }
