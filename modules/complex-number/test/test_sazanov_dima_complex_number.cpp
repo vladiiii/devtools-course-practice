f1ffc9f00fa8cea99c41a0666463c8d31363a6aa
DimaSaz <st132814@gmail.com>
2019-04-13 22:01:31 +0300

    test UPD

---
+++ modules/complex-number/test/test_sazanov_dima_complex_number.cpp
@@ -0,0 +1,72 @@
+// Copyright 2019 Sazanov Dima
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Sazanov_Dima_ComplexNumberTest, Comolex_Number_Can_Create_If_Empty) {
+	// Arrange
+    double n1 = 0.0;
+    double n2 = 0.0;
+    ComplexNumber cnn(n1 , n2);
+    ComplexNumber cnr;
+
+    // Assert
+    EXPECT_EQ(cnn , cnr);
+}
+
+TEST(Sazanov_Dima_ComplexNumberTest, Complex_Number_Can_Copy) {
+    // Arrange
+    double n1 = 13.0;
+    double n2 = 13.0;
+    ComplexNumber cn(n1 , n2);
+
+    // Act
+    ComplexNumber cn1(cn);
+
+    // Assert
+    EXPECT_EQ(cn1 , cn);
+}
+
+TEST(Sazanov_Dima_ComplexNumberTest, Comolex_Number_Can_Create_A_Negative) {
+    // Arrange
+    double n1 = -10.0;
+    double n2 = -11.0;
+
+    // Act
+    ComplexNumber z(n1 , n2);
+
+    // Assert
+    EXPECT_EQ(n1 , z.getRe());
+    EXPECT_EQ(n2 , z.getIm());
+}
+
+TEST(Sazanov_Dima_ComplexNumberTest, Complex_Number_Can_Multiplication) {
+    // Arrange
+    double n1 = 1.0;
+    double n2 = 3.0;
+    double n3 = 3.0;
+    double n4 = -7.0;
+    ComplexNumber cn1(n1 , n2);
+    ComplexNumber cn2(n3 , n4);
+    ComplexNumber cnr;
+    
+    // Act
+    cnr = cn1 * cn2;
+
+    // Assert
+    EXPECT_EQ(26.0 , cnr.getRe() + cnr.getIm());
+}
+
+TEST(Sazanov_Dima_ComplexNumberTest, Complex_Number_Cant_Divide_By_Zero) {
+    // Arrange
+    double n1 = 13.0;
+    double n2 = 37.0;
+    double n3 = 0.0;
+    double n4 = 0.0;
+    ComplexNumber cn1(n1, n2);
+    ComplexNumber cn2(n3, n4);
+
+    // Act & Assert
+    ASSERT_ANY_THROW(cn1 / cn2);
+}
