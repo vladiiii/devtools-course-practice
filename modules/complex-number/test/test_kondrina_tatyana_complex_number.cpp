db1759a4b30951a6e9ea259a82b1b4e7ebb4a1f6
Татьяна Кондрина <43033762+SmallKiwi99@users.noreply.github.com>
2019-04-14 14:13:43 +0300

    Update #3 test for lab1

--- modules/complex-number/test/test_kondrina_tatyana_complex_number.cpp
+++ modules/complex-number/test/test_kondrina_tatyana_complex_number.cpp
@@ -5,59 +5,59 @@
 #include "include/complex_number.h"
 
 TEST(Kondrina_Tatyana_ComplexNumder, Can_GetRe_And_GetIm) {
-    // Arrange 
+    // Arrange
     ComplexNumber z(2.0, 3.0);
 
-    // Assert 
+    // Assert
     EXPECT_EQ(2.0, z.getRe());
     EXPECT_EQ(3.0, z.getIm());
 }
 
 TEST(Kondrina_Tatyana_ComplexNumber, Can_Assign) {
-    // Arrange 
+    // Arrange
     ComplexNumber z;
     ComplexNumber z1(2.0, 3.0);
 
-    // Act 
+    // Act
     z = z1;
 
-    // Assert 
+    // Assert
     EXPECT_EQ(z, z1);
 }
 
 TEST(Kondrina_Tatyana_ComplexNumper, Can_Compare) {
-    // Arrange 
+    // Arrange
     ComplexNumber z;
     ComplexNumber z1(2.0, 3.0);
 
-    // Assert 
+    // Assert
     EXPECT_EQ(false, z == z1);
 
-    // Act 
+    // Act
     z = z1;
 
-    // Assert 
+    // Assert
     EXPECT_EQ(false, z != z1);
 }
 
 TEST(Kondrina_Tatyana_ComplexNumber, Can_Add) {
-    // Arrange 
+    // Arrange
     ComplexNumber z;
     ComplexNumber z1(2.0, 3.0);
 
-    // Assert 
+    // Assert
     EXPECT_EQ(z1, z + z1);
 }
 
 TEST(Kondrina_Tatyana_ComplexNumber, Can_Multi_Some_Numbers) {
-    // Arrange 
+    // Arrange
     ComplexNumber z(2.0, 3.0);
     ComplexNumber z1(3.0, 4.0);
 
-    // Act 
+    // Act
     ComplexNumber z2 = z + z1;
 
-    //Assert 
+    //Assert
     ComplexNumber expected_z(-149.0, 43.0);
     EXPECT_EQ(expected_z, z*z1*z2);
 }
