f6803b409639b067f8794719bfbbb4d4cedeec8f
Pinaev Danil <41565593+PinDanil@users.noreply.github.com>
2019-04-14 19:06:25 +0300

    Last test is deleted

--- modules/complex-number/test/test_pinaev_danil_complex_number.cpp
+++ modules/complex-number/test/test_pinaev_danil_complex_number.cpp
@@ -40,21 +40,3 @@ TEST(Pinaev_Danil_ComplexNumberTest, Can_Add_To_Arr) {
     // Assert
     ASSERT_NO_THROW(A[0] = a);
 }
-
-TEST(Pinaev_Danil_ComplexNumberTest, Some_Weird_Acts_With_Arr) {
-    // Arrange
-    ComplexNumber a, b, c, d;
-    ComplexNumber A[4];
-    A[0] = ComplexNumber(3, 4);
-    A[1] = ComplexNumber(-3, -4);
-    A[2] = ComplexNumber(5, 6);
-    A[3] = ComplexNumber(7, 8);
-
-    // Act
-    c = A[0] + A[1];
-    d = A[2] * A[3];
-    b = c / d;
-
-    // Assert
-    ASSERT_EQ(a, b);
-}
