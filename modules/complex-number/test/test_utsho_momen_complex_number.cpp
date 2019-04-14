de5f6e80f0fc01d26019dca51ab6074a78ad2891
utshomomen <32203690+utshomomen@users.noreply.github.com>
2019-04-14 18:14:58 +0300

    Update test_utsho_momen_complex_number.cpp

--- modules/complex-number/test/test_utsho_momen_complex_number.cpp
+++ modules/complex-number/test/test_utsho_momen_complex_number.cpp
@@ -1,52 +1,52 @@
-﻿// Copyright 2019 Utsho Momen 
+// Copyright 2019 Utsho Momen
 
-#include <gtest/gtest.h> 
+#include <gtest/gtest.h>
 
-#include "include/complex_number.h" 
+#include "include/complex_number.h"
 
 TEST(Utsho_Momen_ComplexNumberTest, Init_Imaginary_To_Zero) {
-	// Arrange & Act
-	ComplexNumber z;
+    // Arrange & Act
+    ComplexNumber z;
 
-	// Assert
-	EXPECT_FLOAT_EQ(0, z.getIm());
+    // Assert
+    EXPECT_FLOAT_EQ(0, z.getIm());
 }
 
 TEST(Utsho_Momen_ComplexNumberTest, Set_Init_Values) {
-	// Arrange & Act
-	ComplexNumber z1 = ComplexNumber(5, -2);
-	ComplexNumber z2 = ComplexNumber(1, -1);
+    // Arrange & Act
+    ComplexNumber z1 = ComplexNumber(5, -2);
+    ComplexNumber z2 = ComplexNumber(1, -1);
 
     // Assert
-	EXPECT_FLOAT_EQ(5, z1.getRe());
-	ASSERT_NO_THROW(-2, z2.im());
+    EXPECT_FLOAT_EQ(5, z1.getRe());
+    ASSERT_NO_THROW(-2, z2.im());
 }
 
 TEST(Utsho_Momen_ComplexNumberTest, Comparison_Equal) {
-    // Arrange 
-	ComplexNumber z1;
-	ComplexNumber z2;
+    // Arrange
+    ComplexNumber z1;
+    ComplexNumber z2;
 
-	// Assert 
-	EXPECT_TRUE(z1 == z2);
+    // Assert
+    EXPECT_TRUE(z1 == z2);
 }
 
 TEST(Utsho_Momen_ComplexNumberTest, Trig_Function_Sin) {
-     // Arrange 
-	 ComplexNumber z1;
+     // Arrange
+     ComplexNumber z1;
 
-     // Assert 
-	ASSERT_NO_THROW(3.4, sin(z1).re());
-	ASSERT_NO_THROW(-1.5, sin(z1).im());
+     // Assert
+     ASSERT_NO_THROW(3.4, sin(z1).re());
+     ASSERT_NO_THROW(-1.5, sin(z1).im());
 }
 
 TEST(Utsho_Momen_ComplexNumberTest, Divide_Double_By_ComplexNumber) {
-	// Arrange & Act
-	ComplexNumber z1;
-	double d = 5.0;
-	ComplexNumber z3 = z1;
+    // Arrange & Act
+    ComplexNumber z1;
+    double d = 5.0;
+    ComplexNumber z3 = z1;
 
-	// Assert 
-	ASSERT_NO_THROW(2.5, z3.re());
-	ASSERT_NO_THROW(-2.5, z3.im());
+    // Assert
+    ASSERT_NO_THROW(2.5, z3.re());
+    ASSERT_NO_THROW(-2.5, z3.im());
 }
