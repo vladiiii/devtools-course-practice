f766d4ee32efd234ad4781653b29413c647cd232
megarungle <megarungle@gmail.com>
2019-04-14 19:12:32 +0300

    Deleted repeated tests

--- modules/complex-number/test/test_utkin_konstantin_complex_number.cpp
+++ modules/complex-number/test/test_utkin_konstantin_complex_number.cpp
@@ -4,15 +4,6 @@
 
 #include "include/complex_number.h"
 
-TEST(Utkin_Konstantin_ComplexNumberTest, Throws_When_Divide_By_Zero) {
-    // Arrange
-    ComplexNumber z1(5.9, 10.9);
-    ComplexNumber z2(-5.9, -10.9);
-    ComplexNumber z3(-3.0, -3.0);
-    // Act & Assert
-    EXPECT_ANY_THROW(z3/(z1+z2));
-}
-
 TEST(Utkin_Konstantin_ComplexNumberTest, Can_Add_Several_Complex_Numbers) {
     // Arrange
     ComplexNumber z1(105.0, -220.0);
@@ -44,22 +35,6 @@ TEST(Utkin_Konstantin_ComplexNumberTest, Can_Assign_To_Itself) {
     ComplexNumber z(19283.7465, -564738.291);
     // Act & Assert
     EXPECT_NO_THROW(z = z);
-}
-
-TEST(Utkin_Konstantin_ComplexNumberTest, Comparing_Equals_Returns_True) {
-    // Arrange
-    ComplexNumber z1(43252.332, -54353.324);
-    ComplexNumber z2(43252.332, -54353.324);
-    // Act & Assert
-    EXPECT_EQ(1, z1 == z2);
-}
-
-TEST(Utkin_Konstantin_ComplexNumberTest, Comparing_Not_Equals_Returns_False) {
-    // Arrange
-    ComplexNumber z1(-53454.98999, 0.00003);
-    ComplexNumber z2(5.00064, -3332.128);
-    // Act & Assert
-    EXPECT_EQ(1, z1 != z2);
 }
 
 TEST(Utkin_Konstantin_ComplexNumberTest, Can_Multiply_Several_Complex_Numbers) {
