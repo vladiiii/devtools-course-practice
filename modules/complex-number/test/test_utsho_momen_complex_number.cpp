26566a609d4f24e3e035de177b6f3f04f31fdffb
utshomomen <32203690+utshomomen@users.noreply.github.com>
2019-04-14 19:47:18 +0300

    Update test_utsho_momen_complex_number.cpp

--- modules/complex-number/test/test_utsho_momen_complex_number.cpp
+++ modules/complex-number/test/test_utsho_momen_complex_number.cpp
@@ -16,7 +16,7 @@ TEST(Utsho_Momen_ComplexNumberTest, Divide_By_Zero) {
     // Arrange
     ComplexNumber z1(45.65, 13.35);
     ComplexNumber z2(0.0, 34.75);
-    
+     
     // Act & Assert
     EXPECT_NO_THROW(z1 / z2);
 }
@@ -39,14 +39,4 @@ TEST(Utsho_Momen_ComplexNumberTest, Double_Real_Number) {
     
     // Assert
     EXPECT_EQ(1.0, real);
-}
-
-TEST(Utsho_Momen_ComplexNumberTest, Divide_Double_By_ComplexNumber) {
-    // Arrange & Act
-    ComplexNumber z1;
-    double d = 5.0;
-    ComplexNumber z3 = z1;
-
-    // Assert 
-    ASSERT_NO_THROW(2.5, z3.re());
 }
