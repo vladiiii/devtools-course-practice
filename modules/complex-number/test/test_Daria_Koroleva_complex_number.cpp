650a39967f9470b4395f5cd0cf9d0552e95c792e
Дарья Королева <silenser667@gmail.com>
2019-04-14 12:18:17 +0300

    Daria Koroleva tests for Complex

---
+++ modules/complex-number/test/test_Daria_Koroleva_complex_number.cpp
@@ -0,0 +1,55 @@
+// Copyright 2019 Daria Koroleva
+
+#include <gtest/gtest.h>
+#include "include/complex_number.h"
+
+TEST(Koroleva_Daria_ComplexNumberTest, Constructor_Complexnumber_Is_OK) 
+{
+	// Arrange
+	ComplexNumber a(1., 143.);
+
+	// Act
+	ComplexNumber b(a);
+
+	// Assert
+	EXPECT_EQ(a, b);
+}
+
+TEST(Koroleva_Daria_ComplexNumberTest, can_divide_by_small_number)
+{
+	// Arrange
+	const double eps = std::numeric_limits<double>::epsilon();
+	ComplexNumber a(-32., 143.);
+	ComplexNumber b(eps, eps);
+
+	// Act
+	// Assert
+	ASSERT_NO_THROW(a/b);
+}
+
+TEST(Koroleva_Daria_ComplexNumberTest, cant_divide_by_zero)
+{
+	// Arrange
+	ComplexNumber a(-32., 24.);
+	ComplexNumber b(0., 0.);
+
+	// Act
+	// Assert
+	ASSERT_ANY_THROW(a / b);
+}
+
+TEST(Koroleva_Daria_ComplexNumberTest, can_multiply_three_complex)
+{
+	// Arrange
+	ComplexNumber a(-32., 24.);
+	ComplexNumber b(0., -12.);
+	ComplexNumber c(-12., 12.);
+
+	// Act
+	ComplexNumber res = a * b * c;
+
+	// Assert
+	EXPECT_EQ(-8064, res.getRe());
+	EXPECT_EQ(-1152, res.getIm());
+
+}
