af9a9ded82e5b23c278d2a72f404716cbf2c2ba3
Татьяна Кондрина <43033762+SmallKiwi99@users.noreply.github.com>
2019-04-14 19:40:48 +0300

    Changed some tests, added new tests

--- modules/complex-number/test/test_kondrina_tatyana_complex_number.cpp
+++ modules/complex-number/test/test_kondrina_tatyana_complex_number.cpp
@@ -13,34 +13,39 @@ TEST(Kondrina_Tatyana_ComplexNumber, Can_GetRe_And_GetIm) {
     EXPECT_EQ(3.0, z.getIm());
 }
 
-TEST(Kondrina_Tatyana_ComplexNumber, Can_Assign) {
+TEST(Kondrina_Tatyana_ComplexNumber, Can_Assign_Some_Numbers) {
     // Arrange
-    ComplexNumber z;
-    ComplexNumber z1(2.0, 3.0);
+    ComplexNumber z1(3.0, 4.0);
+    ComplexNumber z2(2.0, 3.0);
+    ComplexNumber z3(4.0, 5.0);
 
     // Act
-    z = z1;
+    z1 = z2 = z3;
 
     // Assert
-    EXPECT_EQ(z, z1);
+    EXPECT_TRUE(z1 == z3);
 }
 
-TEST(Kondrina_Tatyana_ComplexNumber, Can_Add) {
+TEST(Kondrina_Tatyana_ComplexNumber, Check_Correct_Sequence_Of_Actions) {
     // Arrange
-    ComplexNumber z;
-    ComplexNumber z1(2.0, 3.0);
+    ComplexNumber z1(3.0, 4.0);
+    ComplexNumber z2(4.0, 6.0);
+    ComplexNumber z3(2.0, 3.0);
 
-    // Assert
-    EXPECT_EQ(z1, z + z1);
+    // Act
+    ComplexNumber res = z1 + z2 / z3;
+    ComplexNumber res1 = z2 / z3;
+    res1 = res1 + z1;
+    
+    // Arrange
+    EXPECT_EQ(res, res1);
 }
 
 TEST(Kondrina_Tatyana_ComplexNumber, Can_Multi_Some_Numbers) {
     // Arrange
     ComplexNumber z(2.0, 3.0);
     ComplexNumber z1(3.0, 4.0);
-
-    // Act
-    ComplexNumber z2 = z + z1;
+    ComplexNumber z2(5.0, 7.0);
 
     // Assert
     ComplexNumber expected_z(-149.0, 43.0);
