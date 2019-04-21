296a3ffac0315b89c3ee3c24a92ca80e5db4a14f
Сергей Коннов <sergeyzorro007@gmail.com>
2019-04-21 12:49:23 +0300

    cpplint correct try 5

--- modules/tree/src/tree.cpp
+++ modules/tree/src/tree.cpp
@@ -62,12 +62,12 @@ Tree::Node* Tree::CopyNode(Node* node, Node* copyroot) {
 }
 
 Tree::Node* Tree::DelNode(Node* node, int data) {
-    if (node == nullptr) {
+    if (node == nullptr)
         return nullptr;
-        if (data < node->data)
-            node->left = DelNode(node->left, data);
-        else if (data > node->data)
-            node->right = DelNode(node->right, data);
+    if (data < node->data) {
+        node->left = DelNode(node->left, data);
+    } else if (data > node->data) {
+        node->right = DelNode(node->right, data);
     } else {
         Node *p, *v;
         if (node->left == node->right && node->left == nullptr) {
