f48edab647bb12eef91698f4d67709572dc8b97e
Irina Astafeva <43047589+astafeya@users.noreply.github.com>
2019-04-22 10:22:09 +0300

    fix after travis

--- modules/salary/include/TSalary.h
+++ modules/salary/include/TSalary.h
@@ -5,15 +5,17 @@
 #define MODULES_SALARY_INCLUDE_TSALARY_H_
 
 class TSalary {
-protected:
-    int Salary;      // in month
-    int Norm;        // hours in month
-    int Overtime;    // hours in month
-    int Vacantion;   // hours in month
-    int WorkedHours; // hors in month
-public:
-    TSalary(int _Salary, int _Norm, int _WorkedHours, int Overtime, int Vacantion);
+ protected:
+    int Salary;       // in month
+    int Norm;         // hours in month
+    int Overtime;     // hours in month
+    int Vacantion;    // hours in month
+    int WorkedHours;  // hors in month
+ public:
+    TSalary(int _Salary, int _Norm, 
+            int _WorkedHours, int Overtime, 
+            int Vacantion);
     double Payroll();
 };
 
-#endif // !MODULES_SALARY_INCLUDE_TSALARY_H_
+#endif // MODULES_SALARY_INCLUDE_TSALARY_H_
