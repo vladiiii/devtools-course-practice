0bc33a177e9e49f9d6c9645792cef2f6e033b9ee
and7mw <and7mw@gmail.com>
2019-04-23 23:07:55 +0300

    implement push and pop

--- modules/priority_queue/src/priority_queue.cpp
+++ modules/priority_queue/src/priority_queue.cpp
@@ -24,3 +24,37 @@ PriorityQueue::~PriorityQueue() {
         delete tmp;
     }
 }
+
+void PriorityQueue::push(int prior, int val) {
+    Link *element = create(prior, val);
+    if (first == nullptr)
+        first = last = element;
+    else if (first->priority >= prior) {
+        element->next = first;
+        first = element;
+    }
+    else {
+        Link *tmp = first;
+        while (tmp->next != nullptr || tmp->next->priority < prior)
+            tmp = tmp->next;
+        element->next = tmp->next;
+        tmp->next = element;
+    }
+}
+
+void PriorityQueue::pop() {
+    if (last == nullptr)
+        throw "No elements";
+    if (first == last) {
+        delete last;
+        first = last = nullptr;
+    }
+    else {
+        Link *tmp = first;
+        while (tmp->next != last)
+            tmp = tmp->next;
+        delete last;
+        last = tmp;
+    }
+}
+
