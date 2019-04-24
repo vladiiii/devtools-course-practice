f0111605465f217955dd87d7db8d2b64eeff0a0a
and7mw <and7mw@gmail.com>
2019-04-24 16:15:15 +0300

    added test for other methods

--- modules/priority_queue/test/test_priority_queue.cpp
+++ modules/priority_queue/test/test_priority_queue.cpp
@@ -69,4 +69,110 @@ TEST(PriorityQueueTest, element_with_no_max_and_no_min_priority_push_in_queue) {
     EXPECT_EQ(val2, q.top());
 }
 
+TEST(PriorityQueueTest, cant_pop_element_from_empty_queue) {
+    //Arrange
+    PriorityQueue q;
+    //Act & Assert
+    ASSERT_ANY_THROW(q.pop());
+}
 
+TEST(PriorityQueueTest, can_pop_element_from_queue_with_one_element) {
+    //Arrange
+    PriorityQueue q;
+    int prior = 1;
+    int val = 1;
+    //Act
+    q.push(prior, val);
+    q.pop();
+    //Assert
+    EXPECT_EQ(0, q.size());
+}
+
+TEST(PriorityQueueTest, can_pop_element_from_queue_with_several_elements) {
+    //Arrange
+    PriorityQueue q;
+    int prior1 = 1;
+    int val1 = 1;
+    int prior2 = 2;
+    int val2 = 2;
+    int prior3 = 3;
+    int val3 = 3;
+    //Act
+    q.push(prior1, val1);
+    q.push(prior2, val2);
+    q.push(prior3, val3);
+    q.pop();
+    //Assert
+    EXPECT_EQ(2, q.size());
+}
+
+TEST(PriorityQueueTest, cant_top_element_from_empty_queue) {
+    //Arrange
+    PriorityQueue q;
+    //Act & Assert
+    ASSERT_ANY_THROW(q.top());
+}
+
+TEST(PriorityQueueTest, can_top_element_from_queue_with_one_element) {
+    //Arrange
+    PriorityQueue q;
+    int prior = 1;
+    int val = 1;
+    //Act
+    q.push(prior, val);
+    //Assert
+    EXPECT_EQ(val, q.top());
+}
+
+TEST(PriorityQueueTest, can_top_element_from_queue_with_several_elements) {
+    //Arrange
+    PriorityQueue q;
+    int prior1 = 1;
+    int val1 = 1;
+    int prior2 = 2;
+    int val2 = 2;
+    int prior3 = 3;
+    int val3 = 3;
+    //Act
+    q.push(prior1, val1);
+    q.push(prior2, val2);
+    q.push(prior3, val3);
+    //Assert
+    EXPECT_EQ(val3, q.top());
+}
+
+TEST(PriorityQueueTest, if_queue_empty_return_true) {
+    //Arrange
+    PriorityQueue q;
+    //Act & Assert
+    EXPECT_TRUE(q.empty());
+}
+
+TEST(PriorityQueueTest, if_queue_no_empty_return_false) {
+    //Arrange
+    PriorityQueue q;
+    int prior = 1;
+    int val = 1;
+    //Act
+    q.push(prior, val);
+    //Assert
+    EXPECT_FALSE(q.empty());
+}
+
+TEST(PriorityQueueTest, if_queue_empty_return_0) {
+    //Arrange
+    PriorityQueue q;
+    //Act & Assert
+    EXPECT_EQ(0, q.size());
+}
+
+TEST(PriorityQueueTest, if_queue_no_empty_return_size) {
+    //Arrange
+    PriorityQueue q;
+    int prior = 1;
+    int val = 1;
+    //Act
+    q.push(prior, val);
+    //Assert
+    EXPECT_EQ(1, q.size());
+}
