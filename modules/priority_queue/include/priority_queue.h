de8f5f33dcb1a69a86827343362193247314bf3d
and7mw <and7mw@gmail.com>
2019-04-24 16:42:04 +0300

    fix code style

--- modules/priority_queue/include/priority_queue.h
+++ modules/priority_queue/include/priority_queue.h
@@ -4,22 +4,22 @@
 #define MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_H_
 
 class PriorityQueue {
-    private:
-        struct Link {
-            int priority;
-            int value;
-            Link *next;
-        };
-        Link *first, *last;
-        Link *create(int prior, int val);
-    public:
-        PriorityQueue();
-        ~PriorityQueue();
-        void push(int prior, int val);
-        void pop();
-        int top();
-        bool empty();
-        int size();
+ private:
+    struct Link {
+        int priority;
+        int value;
+        Link *next;
+    };
+    Link *first, *last;
+    Link *create(int prior, int val);
+ public:
+    PriorityQueue();
+    ~PriorityQueue();
+    void push(int prior, int val);
+    void pop();
+    int top();
+    bool empty();
+    int size();
 };
 
 #endif  // MODULES_PRIORITY_QUEUE_INCLUDE_PRIORITY_QUEUE_H_
