35d4b5d060ef270d0557bd08ab91c7861bd7c428
Snewar79 <himera5x@yandex.ru>
2019-04-25 16:13:57 +0300

    bit_array ready to work

---
+++ modules/bit_array/test/test_bit_array.cpp
@@ -0,0 +1,19 @@
+// Copyright 2019 Soluyanov Alexsey
+
+#include <gtest/gtest.h>
+
+#include "include/bit_array.h"
+
+TEST(BitArrayTest, Default_Test) {
+    // Arrange
+
+
+    // Act
+
+
+    // Assert
+    EXPECT_EQ(1.0, 1.0);
+}
+
+
+
