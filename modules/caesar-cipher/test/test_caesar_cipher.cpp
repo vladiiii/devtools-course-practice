8a20b62e2795fb41e17bfd3946ac8507fe6300c2
Vladimir Repin <32306715+mezotaken@users.noreply.github.com>
2019-04-25 22:45:58 +0300

    No-throw tests are combined with correctness tests

--- modules/caesar-cipher/test/test_caesar_cipher.cpp
+++ modules/caesar-cipher/test/test_caesar_cipher.cpp
@@ -22,116 +22,16 @@ class CaesarCipherTest : public ::testing::Test {
 
 
 
-TEST_F(CaesarCipherTest, Can_Encrypt_Empty_Message_With_Zero_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(empty_message, zero_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Empty_Message_With_Positive_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(empty_message, positive_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Empty_Message_With_Negative_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(empty_message, negative_key));
-}
-
-
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Message_With_Zero_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(message, zero_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Message_With_Positive_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(message, positive_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Message_With_Negative_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(message, negative_key));
-}
-
-
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Complex_Message_With_Zero_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(complex_message, zero_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Complex_Message_With_Positive_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(complex_message, positive_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Encrypt_Complex_Message_With_Negative_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Encrypt(complex_message, negative_key));
-}
-
-
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Empty_Message_With_Zero_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(empty_message, zero_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Empty_Message_With_Positive_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(empty_message, positive_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Empty_Message_With_Negative_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(empty_message, negative_key));
-}
-
-
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Message_With_Zero_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(message, zero_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Message_With_Positive_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(message, positive_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Message_With_Negative_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(message, negative_key));
-}
-
-
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Complex_Message_With_Zero_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(complex_message, zero_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Complex_Message_With_Positive_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(complex_message, positive_key));
-}
-
-TEST_F(CaesarCipherTest, Can_Decrypt_Complex_Message_With_Negative_Key) {
-    // Act & Assert
-    EXPECT_NO_THROW(CaesarCipher::Decrypt(complex_message, negative_key));
-}
-
-
-
 TEST_F(CaesarCipherTest, Empty_Message_Encryption_With_Zero_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(empty_message, zero_key));
     EXPECT_EQ(empty_message, CaesarCipher::Encrypt(empty_message, zero_key));
 }
 
 TEST_F(CaesarCipherTest,
        Empty_Message_Encryption_With_Positive_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(empty_message, positive_key));
     EXPECT_EQ(empty_message,
               CaesarCipher::Encrypt(empty_message, positive_key));
 }
@@ -139,6 +39,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Empty_Message_Encryption_With_Negative_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(empty_message, negative_key));
     EXPECT_EQ(empty_message,
               CaesarCipher::Encrypt(empty_message, negative_key));
 }
@@ -147,12 +48,14 @@ TEST_F(CaesarCipherTest,
 
 TEST_F(CaesarCipherTest, Message_Encryption_With_Zero_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(message, zero_key));
     EXPECT_EQ(message, CaesarCipher::Encrypt(message, zero_key));
 }
 
 TEST_F(CaesarCipherTest,
        Message_Encryption_With_Positive_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(message, positive_key));
     EXPECT_EQ(message_positive_encrypt,
               CaesarCipher::Encrypt(message, positive_key));
 }
@@ -160,6 +63,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Message_Encryption_With_Negative_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(message, negative_key));
     EXPECT_EQ(message_negative_encrypt,
               CaesarCipher::Encrypt(message, negative_key));
 }
@@ -169,6 +73,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Complex_Message_Encryption_With_Zero_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(complex_message, zero_key));
     EXPECT_EQ(complex_message,
               CaesarCipher::Encrypt(complex_message, zero_key));
 }
@@ -176,6 +81,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Complex_Message_Encryption_With_Positive_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(complex_message, positive_key));
     EXPECT_EQ(complex_message_positive_encrypt,
               CaesarCipher::Encrypt(complex_message, positive_key));
 }
@@ -183,6 +89,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Complex_Message_Encryption_With_Negative_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Encrypt(complex_message, negative_key));
     EXPECT_EQ(complex_message_negative_encrypt,
               CaesarCipher::Encrypt(complex_message, negative_key));
 }
@@ -192,6 +99,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Empty_Message_Decryption_With_Zero_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(empty_message, zero_key));
     EXPECT_EQ(empty_message,
               CaesarCipher::Decrypt(empty_message, zero_key));
 }
@@ -199,6 +107,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Empty_Message_Decryption_With_Positive_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(empty_message, positive_key));
     EXPECT_EQ(empty_message,
               CaesarCipher::Decrypt(empty_message, positive_key));
 }
@@ -206,6 +115,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Empty_Message_Decryption_With_Negative_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(empty_message, negative_key));
     EXPECT_EQ(empty_message,
               CaesarCipher::Decrypt(empty_message, negative_key));
 }
@@ -214,12 +124,14 @@ TEST_F(CaesarCipherTest,
 
 TEST_F(CaesarCipherTest, Message_Decryption_With_Zero_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(message, zero_key));
     EXPECT_EQ(message, CaesarCipher::Decrypt(message, zero_key));
 }
 
 TEST_F(CaesarCipherTest,
        Message_Decryption_With_Positive_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(message, positive_key));
     EXPECT_EQ(message,
               CaesarCipher::Decrypt(message_positive_encrypt, positive_key));
 }
@@ -227,6 +139,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Message_Decryption_With_Negative_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(message, negative_key));
     EXPECT_EQ(message,
               CaesarCipher::Decrypt(message_negative_encrypt, negative_key));
 }
@@ -236,6 +149,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Complex_Message_Decryption_With_Zero_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(complex_message, zero_key));
     EXPECT_EQ(complex_message,
               CaesarCipher::Decrypt(complex_message, zero_key));
 }
@@ -243,6 +157,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Complex_Message_Decryption_With_Positive_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(complex_message, positive_key));
     EXPECT_EQ(complex_message,
               CaesarCipher::Decrypt(complex_message_positive_encrypt,
                                     positive_key));
@@ -251,6 +166,7 @@ TEST_F(CaesarCipherTest,
 TEST_F(CaesarCipherTest,
        Complex_Message_Decryption_With_Negative_Key_Is_Correct) {
     // Act & Assert
+    EXPECT_NO_THROW(CaesarCipher::Decrypt(complex_message, negative_key));
     EXPECT_EQ(complex_message,
               CaesarCipher::Decrypt(complex_message_negative_encrypt,
                                     negative_key));
@@ -258,13 +174,13 @@ TEST_F(CaesarCipherTest,
 
 
 
-TEST_F(CaesarCipherTest, Big_Key_Encryption_Correct) {
+TEST_F(CaesarCipherTest, Big_Key_Encryption_Is_Correct) {
     // Act & Assert
     EXPECT_EQ(message_positive_encrypt,
               CaesarCipher::Encrypt(message, big_key));
 }
 
-TEST_F(CaesarCipherTest, Big_Key_Decryption_Correct) {
+TEST_F(CaesarCipherTest, Big_Key_Decryption_Is_Correct) {
     // Act & Assert
     EXPECT_EQ(message,
         CaesarCipher::Decrypt(message_positive_encrypt, big_key));
