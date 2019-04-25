0204ca4cfbf08ca0105d23dd1b6138a7d72c1244
Vladimir Repin <32306715+mezotaken@users.noreply.github.com>
2019-04-25 22:48:15 +0300

    Ampersand fix

--- modules/caesar-cipher/include/caesar_cipher.h
+++ modules/caesar-cipher/include/caesar_cipher.h
@@ -7,7 +7,7 @@
 class CaesarCipher {
  public:
     static const int letters_num = 26;
-    static std::string Encrypt(const std::string& message, const int &key);
+    static std::string Encrypt(const std::string& message, const int& key);
     static std::string Decrypt(const std::string& message, const int& key);
  private:
     static std::string Shift(const std::string& message, const int& key);
