80b49cdd624d0e24fc1bfe5a4b46436723611f59
IvanYunin <79103889939@yandex.ru>
2019-04-27 18:03:32 +0300

    sqsh

---
+++ modules/search-binary/include/search_binary.h
@@ -0,0 +1,11 @@
+// Copyright 2019 Yunin Ivan
+
+#ifndef MODULES_SEARCH_BINARY_INCLUDE_SEARCH_BINARY_H_
+#define MODULES_SEARCH_BINARY_INCLUDE_SEARCH_BINARY_H_
+
+class SearchBinary {
+ public:
+    int Search(const int key, const int *arr, const int size);
+};
+
+#endif  // MODULES_SEARCH_BINARY_INCLUDE_SEARCH_BINARY_H_
