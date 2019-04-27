de033be193b7a6d90e8eba775ec999219158b2f6
megarungle <megarungle@gmail.com>
2019-04-27 19:59:54 +0300

    Class implementation

---
+++ modules/quadratic-equation/src/quadratic_equation.cpp
@@ -0,0 +1,52 @@
+// Copyright 2019 Utkin Konstantin
+
+#include "include/quadratic_equation.h"
+
+#include <iostream>
+#include <cmath>
+
+double QuadraticEquation::Discriminant() const {
+    return coef_b * coef_b - 4 * coef_a * coef_c;
+}
+
+QuadraticEquation::QuadraticEquation(double a, double b, double c):
+    coef_a(a), coef_b(b), coef_c(c) {
+    if (a == 0.0 && b == 0.0)
+        throw std::string("It is not a quadratic equation");
+}
+
+double QuadraticEquation::GetX1() const {
+    double x;
+    double d = Discriminant();
+
+    if (d < 0.0)
+        throw std::string("No real solutions");
+
+    if (coef_a == 0.0)
+        x = -coef_c / coef_b;
+    else
+        x = (-coef_b + sqrt(d)) / (2 * coef_a);
+    return x;
+}
+
+double QuadraticEquation::GetX2() const {
+    double x;
+    double d = Discriminant();
+
+    if (d < 0.0)
+        throw std::string("No real solutions");
+
+    if (coef_a == 0.0)
+        x = -coef_c / coef_b;
+    else
+        x = (-coef_b - sqrt(d)) / (2 * coef_a);
+    return x;
+}
+
+std::ostream& operator<<(std::ostream &out,
+    const QuadraticEquation qe) {
+    out << qe.coef_a << std::string("*x^2 + ") <<
+        qe.coef_b << std::string("*x + ") << coef_c
+            std::string(" = 0") << endl;
+    return out;
+}
