a5aa77429b086c1aa5fe3657a057477a12c96b20
Alyona <ryabova.alena99@gmail.com>
2019-04-27 22:52:18 +0300

    fix array

--- modules/calculate-price/include/calculate_price.h
+++ modules/calculate-price/include/calculate_price.h
@@ -3,13 +3,16 @@
 #ifndef MODULES_CALCULATE_PRICE_INCLUDE_CALCULATE_PRICE_H_
 #define MODULES_CALCULATE_PRICE_INCLUDE_CALCULATE_PRICE_H_
 
+#include <array>
+
 class CalculatePrice {
  private:
-    int basket_[5];
+    std::array<int, 5> basket_;
     double DiscountCalculate(const int book);
 
  public:
-    CalculatePrice(int b1, int b2, int b3, int b4, int b5);
+    CalculatePrice(int first_book, int second_book,
+        int third_book, int fourth_book, int fifth_book);
 
     double TotalSum();
 };
--- modules/calculate-price/src/calculate_price.cpp
+++ modules/calculate-price/src/calculate_price.cpp
@@ -5,14 +5,13 @@
 #include <stdio.h>
 #include <string>
 
-CalculatePrice::CalculatePrice(int b1, int b2, int b3, int b4, int b5) {
-    if ((b1 < 0) || (b2 < 0) || (b3 < 0) || (b4 < 0) || (b5 < 0))
+CalculatePrice::CalculatePrice(int first_book, int second_book,
+        int third_book, int fourth_book, int fifth_book) {
+    if ((first_book < 0) || (second_book < 0) ||
+        (third_book < 0) || (fourth_book < 0) || (fifth_book < 0))
         throw std::string("Mustn't be contain negative numbers");
-    basket_[0] = b1;
-    basket_[1] = b2;
-    basket_[2] = b3;
-    basket_[3] = b4;
-    basket_[4] = b5;
+	basket_ = {first_book, second_book, third_book,
+		fourth_book, fifth_book};
 }
 
 double CalculatePrice::DiscountCalculate(const int book) {
@@ -37,10 +36,10 @@ double CalculatePrice::DiscountCalculate(const int book) {
 
 double CalculatePrice::TotalSum() {
     double sum = 0.0;
-    int shop_basket[5];
+    std::array<int, 5> shop_basket;
 
     for (int i = 0; i < 5; i++) {
-        shop_basket[i] = basket_[i];
+		shop_basket[i] = basket_[i];
     }
 
     int books = 0;
@@ -50,15 +49,15 @@ double CalculatePrice::TotalSum() {
     }
 
     while (books) {
-    int count = 0;
-    for (int i = 0; i < 5; i++) {
-        if (shop_basket[i]) {
-            books--;
-            shop_basket[i]--;
-            count++;
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            if (shop_basket[i]) {
+                books--;
+                shop_basket[i]--;
+                count++;
+            }
         }
-    }
-    sum += DiscountCalculate(count);
+        sum += DiscountCalculate(count);
     }
 
     return sum;
