78668fa3b553b1ff57bbab008baa1a0da4f5ffa5
Yury Gaydaychuk <agri1987cola@gmail.com>
2019-04-28 11:02:00 +0300

    code fix

--- modules/quick-sort/src/quick-sort.cpp
+++ modules/quick-sort/src/quick-sort.cpp
@@ -13,38 +13,33 @@ bool QuickSort::checkIncreasing(const int *pArr, const int size) {
     return true;
 }
 
-void QuickSort::mySort(int *pArr, int size){
+void QuickSort::mySort(int *pArr, int size) {
     boundedSort(pArr, 0, size - 1);
 }
 
-void QuickSort::my_swap(int* a, int* b){
+void QuickSort::my_swap(int* a, int* b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-int QuickSort::partition (int *arr, int low, int high){
+int QuickSort::partition(int *arr, int low, int high) {
     int pivot = arr[high];
     int i = (low - 1);
-    for (int j = low; j <= high- 1; j++) 
-    { 
-        if (arr[j] <= pivot) 
-        {
+    for (int j = low; j <= high- 1; j++) {
+        if (arr[j] <= pivot) {
             i++;
-            my_swap(&arr[i], &arr[j]); 
+            my_swap(&arr[i], &arr[j]);
         }
-    } 
-    my_swap(&arr[i + 1], &arr[high]); 
-    return (i + 1); 
+    }
+    my_swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 }
 
-void QuickSort::boundedSort(int *arr, int low, int high){
-    if (low < high) 
-    {
-        int pi = partition(arr, low, high); 
-        boundedSort(arr, low, pi - 1); 
-        boundedSort(arr, pi + 1, high); 
-    } 
+void QuickSort::boundedSort(int *arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        boundedSort(arr, low, pi - 1);
+        boundedSort(arr, pi + 1, high);
+    }
 }
-
-
