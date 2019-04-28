d8db67c47a9f9798ede6ffd514695f5e64d7d988
Yury Gaydaychuk <agri1987cola@gmail.com>
2019-04-28 10:19:12 +0300

    code fix

--- modules/quick-sort/src/quick-sort.cpp
+++ modules/quick-sort/src/quick-sort.cpp
@@ -4,11 +4,47 @@
 
 #include <stdbool.h>
 
-bool QuickSort::checkIncreasing(const int *pArr, const int size){
-    for(int i = 0; i < size - 1; i++){
-        if(pArr[i] > pArr[i + 1]){
+bool QuickSort::checkIncreasing(const int *pArr, const int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (pArr[i] > pArr[i + 1]) {
             return false;
         }
     }
-	return true;
+    return true;
 }
+
+void QuickSort::sort(int *pArr, int size){
+    boundedSort(pArr, 0, size - 1);
+}
+
+void QuickSort::swap(int* a, int* b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int QuickSort::partition (int *arr, int low, int high){
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        if (arr[j] <= pivot) 
+        {
+            i++;
+            swap(&arr[i], &arr[j]); 
+        }
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+}
+
+void QuickSort::boundedSort(int *arr, int low, int high){
+    if (low < high) 
+    {
+        int pi = partition(arr, low, high); 
+        boundedSort(arr, low, pi - 1); 
+        boundedSort(arr, pi + 1, high); 
+    } 
+}
+
+
--- modules/quick-sort/test/test_quick-sort.cpp
+++ modules/quick-sort/test/test_quick-sort.cpp
@@ -7,8 +7,8 @@
 // Checking of simple increasing list
 TEST(Gaydaychuk_Yury_ComplexNumberTest, Increasing_Checking) {
     // Arrange
-	QuickSort qs;
-	int arr[3] = {1, 2, 3};
+    QuickSort qs;
+    int arr[3] = {1, 2, 3};
 
     // Act
     bool res = qs.checkIncreasing(arr, 3);
@@ -16,4 +16,46 @@ TEST(Gaydaychuk_Yury_ComplexNumberTest, Increasing_Checking) {
     // Assert
     EXPECT_TRUE(res);
 }
+
+// Checking of unsorted list
+TEST(Gaydaychuk_Yury_ComplexNumberTest, Unsorted_Checking) {
+    // Arrange
+    QuickSort qs;
+    int arr[3] = {1, 3, 2};
+
+    // Act
+    bool res = qs.checkIncreasing(arr, 3);
+
+    // Assert
+    EXPECT_TRUE(!res);
+}
+
+// Sort function passes checking
+TEST(Gaydaychuk_Yury_ComplexNumberTest, Sort_Passes_Checking) {
+    // Arrange
+    QuickSort qs;
+    int arr[3] = {3, 2, 1};
+
+    // Act
+    qs.sort(arr, 3);
+    bool res = qs.checkIncreasing(arr, 3);
+
+    // Assert
+    EXPECT_TRUE(res);
+}
+
+// First element checking
+TEST(Gaydaychuk_Yury_ComplexNumberTest, First_Element_Checking) {
+    // Arrange
+    QuickSort qs;
+    int arr[3] = {3, 2, 1};
+
+    // Act
+    qs.sort(arr, 3);
+    int must_be_one = arr[0];
+
+    // Assert
+    EXPECT_EQ(1, must_be_one);
+}
+
 
