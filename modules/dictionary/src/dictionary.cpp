ce97123a8b0c3cd80ca4014f8ffdfa0e542cb0cf
Blaginvanya <blaginvanya@mail.ru>
2019-04-29 10:45:37 +0300

    attempt 4.3

--- modules/dictionary/src/dictionary.cpp
+++ modules/dictionary/src/dictionary.cpp
@@ -68,9 +68,7 @@ Node* Dictionary::DoubleRightRotate(Node* t) {
 }
 
 Node* Dictionary::FindMin(Node* t) {
-    if (t == nullptr) {
-        return nullptr;
-    } else if (t->left_ == nullptr) {
+    } if (t->left_ == nullptr) {
         return t;
     } else {
         return FindMin(t->left_);
