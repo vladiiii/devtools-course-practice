b2fd21086aebfc90bfcbf16f948189773d19dff6
Михаил Грибанов <muchroom4427@yandex.ru>
2019-05-10 08:50:11 +0300

    Added header file

---
+++ modules/queue/include/queue.h
@@ -0,0 +1,28 @@
+// Copyright 2019 Gribanov Mihail
+
+#ifndef MODULES_QUEUE_INCLUDE_QUEUE_H_
+#define MODULES_QUEUE_INCLUDE_QUEUE_H_
+
+#include <iostream>
+
+class Queue {
+ private:
+    int tail_;
+    int head_;
+    int * pq_;
+	int size_;
+	int count_;
+
+	int GetNextIndex(int ind) const;
+
+ public:
+    Queue(int s);
+	Queue(const Queue &q);
+	~Queue();
+
+	void Put(int &val);
+	int Get();
+	int TopElem() const;
+	int GetCount() const;
+};
+#endif  // MODULES_QUEUE_INCLUDE_QUEUE_H_
