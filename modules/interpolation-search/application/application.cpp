8e2ef00bf39f7a3ae028c30b5780b68b7ba3a83b
ivanvikhrev <l4ind@mail.ru>
2019-05-11 22:03:07 +0300

    initial

---
+++ modules/interpolation-search/application/application.cpp
@@ -0,0 +1,8 @@
+// Copyright 2019 Vikhrev Ivan
+
+#include <iostream>
+#include "include/InterpolationSearch.h"
+
+int main(int argc, char const ** argv) {
+     std::cout << "Hello World" << std::endl;
+}
