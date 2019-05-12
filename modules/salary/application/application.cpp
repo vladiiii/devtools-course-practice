badf10db132b18e0d07057066abfb153fafa7aef
Alyona <ryabova.alena99@gmail.com>
2019-05-12 15:00:39 +0300

    Fix main

--- modules/salary/application/application.cpp
+++ modules/salary/application/application.cpp
@@ -1,14 +1,11 @@
 // Copyright 2019 Ryabova Alyona
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
+#include <iostream>
 
 #include "include/salary_application.h"
 
 int main(int argc, const char** argv) {
     SalaryApplication app;
-    std::string output = app(argc, argv);
-    printf("%s\n", output.c_str());
+    std::cout << app(argc, argv);
     return 0;
 }
