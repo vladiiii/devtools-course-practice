5fd01c0bf5a5d51b1d740198bcd87bb24fa523ef
Pahandrovich <p_yak_11@mail.ru>
2019-05-12 15:32:00 +0300

    add yakovlev lab3

---
+++ modules/elasticity_of_demand/include/demand_app.h
@@ -0,0 +1,28 @@
+// Copyright 2019 Yakovlev Pavel
+
+#ifndef MODULES_ELASTICITY_OF_DEMAND_INCLUDE_DEMAND_APP_H_
+#define MODULES_ELASTICITY_OF_DEMAND_INCLUDE_DEMAND_APP_H_
+
+#include "include/elasticity_of_demand.h"
+
+#include <string>
+#include <cstdlib>
+
+class DemandApp {
+ public:
+    DemandApp();
+    std::string operator()(int argc, const char** argv);
+
+ private:
+    void help(const char* appname, const char* message = "");
+    bool validateNumberOfArguments(int argc, const char** argv);
+    std::string message_;
+    typedef struct {
+        double price1, price2;
+        int quantity1, quantity2;
+        double income1, income2;
+        char operation;
+    } Arguments;
+};
+
+#endif  // MODULES_ELASTICITY_OF_DEMAND_INCLUDE_DEMAND_APP_H_
---
+++ modules/elasticity_of_demand/src/demand_app.cpp
@@ -0,0 +1,123 @@
+// Copyright 2019 Yakovlev Pavel
+
+#include "include/elasticity_of_demand.h"
+#include "include/demand_app.h"
+
+#include <stdio.h>
+#include <cstdlib>
+#include <stdint.h>
+#include <string>
+#include <sstream>
+
+DemandApp::DemandApp() : message_("") {}
+
+void DemandApp::help(const char* appname, const char* message) {
+    message_ =
+        std::string(message) +
+          "This is a elasticity of demand application.\n\n" +
+          "Please provide arguments in the following format:\n\n"+
+
+          "  $ " + appname + " <price1> <price2> " +
+          "<quantity1> <quantity2>" +
+          "<income2> <income2>" +
+          " <operation>\n\n" +
+
+          "Where all arguments are double-precision numbers and integers, " +
+          "where <operation> :\n" +
+          "p - Price Elasticity Of Demand\n" +
+          "i - Income Elasticity Of Demand\n" +
+          "c - Cross Elasticity Of Demand";
+}
+
+bool DemandApp::validateNumberOfArguments(int argc,
+                                                    const char** argv) {
+    if (argc == 1) {
+        help(argv[0]);
+        return false;
+    } else if (argc != 8) {
+        help(argv[0], "ERROR: Should be 7 arguments.\n\n");
+        return false;
+    }
+    return true;
+}
+
+double ParseDouble(const char* arg) {
+    char* end;
+    double value = strtod(arg, &end);
+
+    if (end[0]) {
+        throw std::string("Wrong number format!");
+    }
+
+    return value;
+}
+
+int ParseInt(const char* arg) {
+    char* end;
+    int value = strtol(arg, &end, 10);
+
+    if (end[0]) {
+        throw std::string("Wrong number format!");
+    }
+
+    return value;
+}
+
+int ParseOperation(const char* arg) {
+    int op;
+    if (strcmp(arg, "p") == 0) {
+        op = 1;
+    } else if (strcmp(arg, "i") == 0) {
+        op = 2;
+    } else if (strcmp(arg, "c") == 0) {
+        op = 3;
+    } else {
+        throw std::string("Wrong operation format!");
+    }
+    return op;
+}
+
+std::string DemandApp::operator()(int argc, const char** argv) {
+    Arguments args;
+
+    if (!validateNumberOfArguments(argc, argv)) {
+        return message_;
+    }
+    try {
+        args.price1    = ParseDouble(argv[1]);
+        args.price2    = ParseDouble(argv[2]);
+        args.quantity1 = ParseInt(argv[3]);
+        args.quantity2 = ParseInt(argv[4]);
+        args.income1   = ParseDouble(argv[5]);
+        args.income2   = ParseDouble(argv[6]);
+        args.operation = ParseOperation(argv[7]);
+    }
+    catch(std::string& str) {
+        return str;
+    }
+
+    ElasticityOfDemand obj;
+
+    double res;
+    std::ostringstream stream;
+    switch (args.operation) {
+     case 1:
+        res = obj.PriceElasticityOfDemand(args.quantity1,
+            args.quantity2, args.price1, args.price2);
+        stream << "Result = " << res;
+        break;
+     case 2:
+        res = obj.IncomeElasticityOfDemand(args.quantity1,
+            args.quantity2, args.income1, args.income2);
+        stream << "Result = " << res;
+        break;
+     case 3:
+        res = obj.CrossElasticityOfDemand(args.quantity1,
+            args.quantity2, args.price1, args.price2);
+        stream << "Result = " << res;
+    }
+
+    message_ = stream.str();
+
+    return message_;
+}
