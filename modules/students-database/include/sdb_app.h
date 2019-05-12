b629b6ffcd33762e26abd99112adba7563423f48
Matt <mat3580@gmail.com>
2019-05-12 16:30:53 +0300

    Lint

--- modules/students-database/include/sdb_app.h
+++ modules/students-database/include/sdb_app.h
@@ -1,18 +1,18 @@
-// Copyright 2019 Kutovoi Vadim
+// Copyright 2019 Kudrin Matvey
 
+#include <string>
 
 #ifndef DEVTOOLS_SDB_APP_H
 #define DEVTOOLS_SDB_APP_H
 
 class SdbApp {
-        public:
-        SdbApp();
-        std::string operator()(int argc, const char** argv);
-        std::string Help(const char* appname);
+ public:
+    SdbApp();
+    std::string operator()(int argc, const char** argv);
+    std::string Help(const char* appname);
 
-        private:
-        std::string message_;
-
+ private:
+    std::string message_;
 };
 
-#endif //DEVTOOLS_SDB_APP_H
+#endif  // MODULES_STUDENTS_DATABASE_INCLUDE_SDB_APP_H_
