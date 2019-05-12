052665972c022128ed8491fd6f07a38333610ae9
Дарья Королева <silenser667@gmail.com>
2019-05-12 11:48:40 +0300

    fixed tests names cutted v2

--- modules/complex-number/test/test_Daria_Koroleva_complex_number.cpp
+++ modules/complex-number/test/test_Daria_Koroleva_complex_number.cpp
@@ -4,7 +4,7 @@
 #include <limits>
 #include "include/complex_number.h"
 
-TEST(Koroleva_Daria_ComplexNumberTest, Can_Create_Complex_With_CopyConstructor) {
+TEST(Koroleva_Daria_ComplexNumberTest, Can_Create_Complex_By_CopyConstructor) {
     // Arrange
     ComplexNumber a(1., 143.);
     // Act
