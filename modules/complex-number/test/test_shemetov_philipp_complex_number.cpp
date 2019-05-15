fab4ced05794cff23b79d4dcedab76d76847efd9
Philipp <filshe1512@gmail.com>
2019-05-15 17:46:45 +0300

    Update

--- modules/complex-number/test/test_shemetov_philipp_complex_number.cpp
+++ modules/complex-number/test/test_shemetov_philipp_complex_number.cpp
@@ -17,7 +17,7 @@ TEST(Shemetov_Philipp_ComplexNumberTest, Addition_Real_Numbers) {
     EXPECT_TRUE(result.getRe() == 10.0 && result.getIm() == 0.0);
 }
 
-TEST(Shemetov_Philipp_ComplexNumberTest, Multiply_On_Zero) {
+TEST(Shemetov_Philipp_ComplexNumberTest, Multiply_By_Zero) {
     // Arrange
     ComplexNumber zero(0, 0);
     ComplexNumber second(10.0, 12.0);
@@ -30,17 +30,15 @@ TEST(Shemetov_Philipp_ComplexNumberTest, Multiply_On_Zero) {
     EXPECT_FLOAT_EQ(0, res.getIm());
 }
 
-TEST(Shemetov_Philipp_ComplexNumberTest, Equate_Complexnumber) {
+TEST(Shemetov_Philipp_ComplexNumberTest, FalseComparingDiffComplNum) {
     // Arrange
-    ComplexNumber first(10.2345, 15.0);
-    ComplexNumber second(275.92, 34.35);
-    ComplexNumber third(10567.79, 103456.38);
-
+    ComplexNumber z1(10.0, 0.0);
+    ComplexNumber z2(10.0, 0.0);
+    ComplexNumber z3(0.0, 0.1);
     // Act
-    first = second = third;
-
+    z1 = z2 + z3;
     // Assert
-    EXPECT_TRUE(third == first && second == first);
+    EXPECT_FALSE(z1 == z2);
 }
 
 TEST(Shemetov_Philipp_ComplexNumberTest, Sum_Two_Opposite_Equals_Zero) {
