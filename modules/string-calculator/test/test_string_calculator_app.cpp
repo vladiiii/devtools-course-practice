de1a5742d87141a5bce92598e5e9eda5a653f785
likholat <aniala201398@gmail.com>
2019-05-16 19:07:33 +0300

    tests for string_calculator_app were changed

--- modules/string-calculator/test/test_string_calculator_app.cpp
+++ modules/string-calculator/test/test_string_calculator_app.cpp
@@ -136,12 +136,12 @@ TEST_F(StringCalculatorAppTest, Can_Do_Several_Operations) {
 }
 
 TEST_F(StringCalculatorAppTest, Can_Use_Brackets) {
-    string str = "(1+1)";
+    string str = "(1+1+1)";
     vector<string> args = { str };
 
     Act(args);
 
-    Assert("Result = 6");
+    Assert("Result = 3");
 }
 
 TEST_F(StringCalculatorAppTest, Can_Not_Calculate_Mismatched_Brackets) {
