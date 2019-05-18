ddef69db628d4ae7b7cfa75d52a7b55e00380e03
Yury Gaydaychuk <agri1987cola@gmail.com>
2019-05-18 16:46:15 +0300

    fix

--- modules/print-simple-numbers/src/prime_counter.cpp
+++ modules/print-simple-numbers/src/prime_counter.cpp
@@ -70,11 +70,10 @@ std::string PrimeCounter::operator()(int argc, const char** argv) {
 
     if (res == -1) {
         stream << "No prime numbers in selected interval.";
-    }
-    else {
+    } else {
         stream << "Count of prime numbers between "
-    	   << args.left_boundary << " and  "
-    	   << args.right_boundary << " equal " << res;
+           << args.left_boundary << " and  "
+           << args.right_boundary << " equal " << res;
     }
 
     message_ = stream.str();
