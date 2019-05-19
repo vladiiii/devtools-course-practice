355a74757557a136c4a93662eeb1a9d7df9a520f
alinaut <bykovskaya.alina@gmail.com>
2019-05-19 14:43:31 +0300

    code style fix 3

--- modules/priority_queue/test/test_priority_queue_application.cpp
+++ modules/priority_queue/test/test_priority_queue_application.cpp
@@ -10,7 +10,7 @@ using std::vector;
 using std::string;
 
 class PriorAppTest : public ::testing::Test {
-protected:
+ protected:
     void Act(vector<string> args_) {
         vector<const char*> options;
 
@@ -28,7 +28,7 @@ protected:
         EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
     }
 
-private:
+ private:
     PriorityApp app_;
     string output_;
 };
