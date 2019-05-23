681664061aceb5ecde846b95f3d1d50d03208cb1
Irina Astafeva <astafeya99@gmail.com>
2019-05-23 19:06:02 +0300

    Fix after travis

--- modules/calculate-price/include/calculate_price_application.h
+++ modules/calculate-price/include/calculate_price_application.h
@@ -6,21 +6,21 @@
 #include <string>
 
 class CalculatePriceApplication {
-    public:
-        CalculatePriceApplication() = default;
-        std::string operator()(int argc, const char ** argv);
-    private:
-        void Help(const char *appname);
-        bool ValidateNambersOfArguments(int argc, const char ** argv);
-        bool IsArgumentsInteger(const char ** argv);
-        std::string message_;
-        typedef struct {
-            int book_1;
-            int book_2;
-            int book_3;
-            int book_4;
-            int book_5;
-        } Arguments;
+ public:
+    CalculatePriceApplication() = default;
+    std::string operator()(int argc, const char ** argv);
+ private:
+    void Help(const char *appname);
+    bool ValidateNambersOfArguments(int argc, const char ** argv);
+    bool IsArgumentsInteger(const char ** argv);
+    std::string message_;
+    typedef struct {
+        int book_1;
+        int book_2;
+        int book_3;
+        int book_4;
+        int book_5;
+    } Arguments;
 };
 
 #endif  // MODULES_CALCULATE_PRICE_INCLUDE_CALCULATE_PRICE_APPLICATION_H_
