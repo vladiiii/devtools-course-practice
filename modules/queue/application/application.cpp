eff1f7e8571636f33109234f5a72dbf5e3ecf602
megarungle <megarungle@gmail.com>
2019-05-24 17:56:42 +0300

    Initial commit

---
+++ modules/queue/application/application.cpp
@@ -0,0 +1,7 @@
+// Copyright 2019 Utkin Konstantin
+
+#include <iostream>
+
+int main(int argc, const char** argv) {
+    std::cout << "Hello world!" << std::endl;
+}
---
+++ modules/queue/test/test_queue_app.cpp
@@ -0,0 +1,7 @@
+// Copyright 2019 Utkin Konstantin
+
+#include <gtest/gtest.h>
+
+TEST(Test, True_Is_True) {
+    EXPECT_EQ(true, true);
+}
