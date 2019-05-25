c285c845ad8de10b0136df6c88567f5f40ea22b6
Михаил Грибанов <muchroom4427@yandex.ru>
2019-05-25 20:07:41 +0300

    Fixed indent

--- modules/quadratic-equation/include/quadratic_equation_application.h
+++ modules/quadratic-equation/include/quadratic_equation_application.h
@@ -9,13 +9,13 @@
 class QuadraticEquationApplication {
 
 public:
-    QuadraticEquationApplication() = default;
-    std::string operator()(int argc, const char** argv);
+     QuadraticEquationApplication() = default;
+     std::string operator()(int argc, const char** argv);
 
  private:
      std::string message_;
      int coef[3];
-     void Help(const char* appname, const char* message = "");    
-     bool CheckArguments(int argc, const char** argv); 
+     void Help(const char* appname, const char* message = "");
+     bool CheckArguments(int argc, const char** argv);
 };
 #endif  // MODULES_QUADRATIC_EQUATION_INCLUDE_QUADRATIC_EQUATION_APPLICATION_H_
